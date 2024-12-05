067edce93d73236f7ed11803f3d3f71ba24affd9
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-12-05 09:39:33 -0600

    add driver summary to availability module

--- src/router/PrivateRoutes.tsx
+++ src/router/PrivateRoutes.tsx
@@ -1,57 +1,76 @@
 import 'react-toastify/dist/ReactToastify.css';
-import '../theme.min.css';
 
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Suspense, lazy, useEffect } from 'react';
 
 import AccesoForm from '../phicargo/accesos/formulario';
 import Accesos from '../phicargo/accesos/Accesos';
 import AsignacionUnidades from '../phicargo/reportes/asignacion_unidades';
 import AvailabilityLayout from '../phicargo/modules/availability/layout/AvailabilityLayout';
+import AvailabilityRoutes from '../phicargo/modules/availability/routes/AvailabilityRoutes';
 import CartasPorte from '../phicargo/maniobras/tms_waybill/cartas_porte';
-import ControlUsuarios from '../phicargo/usuarios/ControlUsuarios';
+import ControlEstatusOperativos from '../phicargo/control_estatus/ControlEstatusOperativos';
+import ControlManiobras from '../phicargo/maniobras/control/control';
+import ControlOperadores from '../phicargo/operadores/ControlUsuarios';
 import ControlViajesActivos from '../phicargo/viajes/control';
 import ControlViajesFinalizados from '../phicargo/viajes/finalizados';
 import ControlViajesProgramados from '../phicargo/viajes/programacion';
 import CorreosElectronicos from '../phicargo/correos_electronicos/correos_electronicos';
 import DetencionesTable from '../phicargo/reportes/llegadas_tarde/llegadas_tarde';
 import EntregaMonitoreo from '../phicargo/monitoreo/monitoreo';
-import { LoadingPage } from '../phicargo/modules/core/pages/LoadingPage';
 import Menu from '../phicargo/menu/menu';
 import Nominas from '../phicargo/maniobras/pagos/pagos';
 import PersistentDrawer from '../phicargo/monitoreo/Eventos';
 import Precios_maniobras from '../phicargo/maniobras/precios/precios';
+import ProtectedRoute from './ProtectedRoute';
 import ReporteCumplimiento from '../phicargo/reportes/cumplimiento';
+import ShiftsLayout from '../phicargo/modules/shifts/layouts/ShiftsLayout';
+import ShiftsRoutes from '../phicargo/modules/shifts/routes/ShiftsRoutes';
 import Terminales from '../phicargo/maniobras/maniobras/terminales/registros';
-import ControlOperadores from '../phicargo/operadores/ControlUsuarios';
 import { ToastContainer } from 'react-toastify';
 import { Toaster } from 'react-hot-toast';
-import ControlEstatusOperativos from '../phicargo/control_estatus/ControlEstatusOperativos';
-import control_maniobras from '../phicargo/maniobras/control/control';
-import ControlManiobras from '../phicargo/maniobras/control/control';
-import ControlTurnos from '../phicargo/turnos/ControlTurnos';
+import UsersManagementLayout from '../phicargo/modules/users-management/layouts/UsersManagementLayout';
+import UsersManagementRoutes from '../phicargo/modules/users-management/routes/UsersManagementRoutes';
+import { useEffect } from 'react';
 
-// Lazy loading pages
-// Availability Pages and Outlets
-const VehicleAvailabilityPage = lazy(
-  () =>
-    import('../phicargo/modules/availability/pages/VehicleAvailabilityPage'),
-);
-const VehicleInfo = lazy(
-  () => import('../phicargo/modules/availability/outlets/VehicleInfo'),
-);
-const DriverAvailabilityPage = lazy(
-  () => import('../phicargo/modules/availability/pages/DriverAvailabilityPage'),
-);
-const DriverInfo = lazy(
-  () => import('../phicargo/modules/availability/outlets/DriverInfo'),
-);
-const NotAssignedPage = lazy(
-  () => import('../phicargo/modules/availability/pages/NotAssignedPage'),
-);
-const SummaryPage = lazy(
-  () => import('../phicargo/modules/availability/pages/SummaryPage'),
-);
+const PERMISSIONS = {
+  'Módulo trafico': 1,
+  'Módulo ajustes': 2,
+  'Módulo bonos': 3,
+  'Módulo reportes': 4,
+  'Módulo usuarios': 5,
+  'Módulo operadores': 7,
+  'Modulo turnos': 8,
+  'Ingresar incidencia asesor legal': 9,
+  'Ingresar incidencia mantenimiento': 10,
+  'Ingresar incidencia th': 11,
+  'Ingresar incidencia operaciones': 12,
+  'Ingresar incidencia calidad': 13,
+  'modulo maniobras': 38,
+  'modulo comunicados': 39,
+  'modulo monitoreo': 40,
+  'cuentas operadores': 41,
+  viajes: 101,
+  finalizados: 102,
+  'banco de correos': 103,
+  detenciones: 104,
+  'pestañas viajes': 105,
+  retorno: 106,
+  'modulo informe': 120,
+  'informe empresa phicargo': 121,
+  'informe empresa servicontainer': 122,
+  'informe empresa tankcontainer': 123,
+  'informe empresa ometra': 124,
+  'informe empresa transportes belchez': 125,
+  'modulo accesos': 126,
+  'modulo ti': 127,
+  'reporte gerencial': 190,
+  'reporte ingresos': 191,
+  'numero de viajes': 192,
+  'inicios de ruta y llegada a planta': 193,
+  estadias: 195,
+  'cumplimiento estatus': 196,
+};
+
 
 export const PrivateRoutes = () => {
   useEffect(() => {
@@ -79,89 +98,70 @@ export const PrivateRoutes = () => {
         <Route path="/" element={<Navigate to="/menu" />} />
 
         <Route path="/menu" element={<Menu />} />
-        <Route path="/turnos" element={<ControlTurnos />} />
 
         <Route path="/cartas-porte" element={<CartasPorte />} />
-        <Route path="/control_maniobras" element={<ControlManiobras></ControlManiobras>} />
+        <Route path="/control_maniobras" element={<ControlManiobras />} />
         <Route path="/nominas" element={<Nominas />} />
         <Route path="/precios" element={<Precios_maniobras />} />
         <Route path="/terminales" element={<Terminales />} />
 
         <Route path="/Viajes" element={<ControlViajesActivos />} />
-        <Route path="/ViajesFinalizados" element={<ControlViajesFinalizados />} />
-        <Route path="/ViajesProgramados" element={<ControlViajesProgramados />} />
+        <Route
+          path="/ViajesFinalizados"
+          element={<ControlViajesFinalizados />}
+        />
+        <Route
+          path="/ViajesProgramados"
+          element={<ControlViajesProgramados />}
+        />
         <Route path="/controlestatus" element={<ControlEstatusOperativos />} />
 
-        <Route path="/CorreosElectronicos" element={<CorreosElectronicos estado={undefined} />} />
+        <Route
+          path="/CorreosElectronicos"
+          element={<CorreosElectronicos estado={undefined} />}
+        />
 
         <Route path="/cumplimiento" element={<ReporteCumplimiento />} />
 
         <Route path="/Accesos" element={<Accesos />} />
-        <Route path="/AccesoForm" element={<AccesoForm id_acceso={undefined} onClose={undefined} />} />
+        <Route
+          path="/AccesoForm"
+          element={<AccesoForm id_acceso={undefined} onClose={undefined} />}
+        />
         <Route path="/Monitoreo" element={<EntregaMonitoreo />} />
-        <Route path="/Monitorista" element={<PersistentDrawer id_entrega={undefined} onClose={undefined} />} />
+        <Route
+          path="/Monitorista"
+          element={
+            <PersistentDrawer id_entrega={undefined} onClose={undefined} />
+          }
+        />
 
         <Route path="/detenciones" element={<DetencionesTable />} />
         <Route path="/asignacion" element={<AsignacionUnidades />} />
 
-        <Route path="/usuarios" element={<ControlUsuarios />} />
         <Route path="/controloperadores" element={<ControlOperadores />} />
 
-        <Route path="/disponibilidad" element={<AvailabilityLayout children={undefined} />}>
-          <Route
-            index
-            element={<Navigate to="/disponibilidad/unidades" replace />}
-          />
-          <Route
-            path="unidades"
-            element={
-              <Suspense fallback={<LoadingPage />}>
-                <VehicleAvailabilityPage />
-              </Suspense>
-            }
-          >
-            <Route
-              path="detalles/:id"
-              element={
-                <Suspense fallback={<LoadingPage />}>
-                  <VehicleInfo />
-                </Suspense>
-              }
+        <Route
+          path="/control-usuarios"
+          element={
+            <ProtectedRoute
+              element={<UsersManagementLayout children={undefined} />}
+              requiredPermissionId={PERMISSIONS['Módulo usuarios']}
             />
-          </Route>
-          <Route
-            path="operadores"
-            element={
-              <Suspense fallback={<LoadingPage />}>
-                <DriverAvailabilityPage />
-              </Suspense>
-            }
-          >
-            <Route
-              path="detalles/:id"
-              element={
-                <Suspense fallback={<LoadingPage />}>
-                  <DriverInfo />
-                </Suspense>
-              }
-            />
-          </Route>
-          <Route
-            path="resumen"
-            element={
-              <Suspense fallback={<LoadingPage />}>
-                <SummaryPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="sin-asignar"
-            element={
-              <Suspense fallback={<LoadingPage />}>
-                <NotAssignedPage />
-              </Suspense>
-            }
-          />
+          }
+        >
+          {UsersManagementRoutes()}
+        </Route>
+
+        <Route
+          path="/disponibilidad"
+          element={<AvailabilityLayout children={undefined} />}
+        >
+          {AvailabilityRoutes()}
+        </Route>
+
+        <Route path="/turnos" element={<ShiftsLayout children={undefined} />}>
+          {ShiftsRoutes()}
         </Route>
 
         {/* Ruta para manejar rutas no válidas */}
