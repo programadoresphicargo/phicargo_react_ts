d363c03884bd797f6bf788f502e4db49688dea5a
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-12-14 09:28:55 -0600

    fix MainMenu responsive

--- src/phicargo/menu/MainManuPage.tsx
+++ src/phicargo/menu/MainManuPage.tsx
@@ -1,3 +1,4 @@
+import AvatarProfile from '../modules/core/components/ui/AvatarProfile';
 import MenuItem from './MenuItem';
 import accesos_img from '../../assets/menu/accesos.png';
 import bonos_img from '../../assets/menu/bonos.png';
@@ -17,73 +18,112 @@ type MenuItemType = {
   label: string;
   link: string;
   requiredPermissions: number[];
-}
+};
 
 const menuItems: MenuItemType[] = [
-  { icon: turnos_img, label: 'Turnos', link: '/terminales', requiredPermissions: [8] },
-  { icon: viajes_img, label: 'Control de viajes', link: '/viajes', requiredPermissions: [1,101,102] },
-  { icon: maniobras_img, label: 'Maniobras', link: '/cartas-porte', requiredPermissions: [38] },
-  { icon: monitoreo_img, label: 'Monitoreo', link: '/monitoreo', requiredPermissions: [40] },
-  { icon: accesos_img, label: 'Accesos', link: '/accesos', requiredPermissions: [126] },
+  {
+    icon: turnos_img,
+    label: 'Turnos',
+    link: '/terminales',
+    requiredPermissions: [8],
+  },
+  {
+    icon: viajes_img,
+    label: 'Control de viajes',
+    link: '/viajes',
+    requiredPermissions: [1, 101, 102],
+  },
+  {
+    icon: maniobras_img,
+    label: 'Maniobras',
+    link: '/cartas-porte',
+    requiredPermissions: [38],
+  },
+  {
+    icon: monitoreo_img,
+    label: 'Monitoreo',
+    link: '/monitoreo',
+    requiredPermissions: [40],
+  },
+  {
+    icon: accesos_img,
+    label: 'Accesos',
+    link: '/accesos',
+    requiredPermissions: [126],
+  },
   { icon: bonos_img, label: 'Bonos', link: '/bonos', requiredPermissions: [3] },
-  { icon: usuarios_img, label: 'Usuarios', link: '/control-usuarios', requiredPermissions: [5] },
-  { icon: operadores_img, label: 'Operadores', link: '/controloperadores', requiredPermissions: [7] },
-  { icon: viajes_img, label: 'Disponibilidad', link: '/disponibilidad', requiredPermissions: [200] },
+  {
+    icon: usuarios_img,
+    label: 'Usuarios',
+    link: '/control-usuarios',
+    requiredPermissions: [5],
+  },
+  {
+    icon: operadores_img,
+    label: 'Operadores',
+    link: '/controloperadores',
+    requiredPermissions: [7],
+  },
+  {
+    icon: viajes_img,
+    label: 'Disponibilidad',
+    link: '/disponibilidad',
+    requiredPermissions: [200],
+  },
   {
     icon: correo_img,
     label: 'Correos electronicos',
     link: '/CorreosElectronicos',
-    requiredPermissions: []
+    requiredPermissions: [],
   },
-  { icon: reportesImg, label: 'Reportes', link: '/reportes', requiredPermissions: [4] },
+  {
+    icon: reportesImg,
+    label: 'Reportes',
+    link: '/reportes',
+    requiredPermissions: [4],
+  },
 ];
 
 const MainMenuPage = () => {
-
   const { session } = useAuthContext();
 
-  const filteredMenuItems = useMemo(() => menuItems.filter((item) =>
-    item.requiredPermissions.every((permission) =>
-      session?.user?.permissions?.includes(permission)
-    )
-  ),[session]);
+  const filteredMenuItems = useMemo(
+    () =>
+      menuItems.filter((item) =>
+        item.requiredPermissions.every((permission) =>
+          session?.user?.permissions?.includes(permission),
+        ),
+      ),
+    [session],
+  );
 
   return (
-    <main className="main">
-      <div
-        className="fixed top-0 left-0 right-0 bg-cover bg-no-repeat"
-        style={{
-          height: '32rem',
-          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 400"><rect width="1920" height="400" fill="%23D9DEEA" /><mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="1920" height="400"><rect width="1920" height="400" fill="%23D9DEEA" /></mask><g mask="url(%23mask0)"><path d="M1059.48 308.024C1152.75 57.0319 927.003 -103.239 802.47 -152.001L1805.22 -495.637L2095.53 351.501L1321.23 616.846C1195.12 618.485 966.213 559.015 1059.48 308.024Z" fill="%23C0CBDD" /><path d="M1333.22 220.032C1468.66 -144.445 1140.84 -377.182 960 -447.991L2416.14 -947L2837.71 283.168L1713.32 668.487C1530.19 670.868 1197.78 584.509 1333.22 220.032Z" fill="%238192B0" /></g></svg>')`,
-        }}
-      >
-        <div className="shape-bottom absolute inset-x-0 bottom-0">
-          <svg
-            preserveAspectRatio="none"
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            viewBox="0 0 1921 273"
-          >
-            <polygon fill="#fff" points="0,273 1921,273 1921,0" />
-          </svg>
+    <div
+      className="min-h-screen bg-gray-100 p-4 flex flex-col justify-between relative"
+      style={{
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 400"><rect width="1920" height="400" fill="%23D9DEEA" /><mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="1920" height="400"><rect width="1920" height="400" fill="%23D9DEEA" /></mask><g mask="url(%23mask0)"><path d="M1059.48 308.024C1152.75 57.0319 927.003 -103.239 802.47 -152.001L1805.22 -495.637L2095.53 351.501L1321.23 616.846C1195.12 618.485 966.213 559.015 1059.48 308.024Z" fill="%23C0CBDD" /><path d="M1333.22 220.032C1468.66 -144.445 1140.84 -377.182 960 -447.991L2416.14 -947L2837.71 283.168L1713.32 668.487C1530.19 670.868 1197.78 584.509 1333.22 220.032Z" fill="%238192B0" /></g></svg>')`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top',
+      }}
+    >
+      <div className="flex items-center mb-8 sm:mb-12 px-4 relative z-10">
+        <div className="flex-grow flex justify-center items-center">
+          <img
+            src="https://phi-cargo.com/wp-content/uploads/2021/05/logo-phicargo-vertical.png"
+            alt="Logo Phi Cargo"
+            className="w-40 sm:w-60 md:w-80 lg:w-[400px] h-auto object-contain"
+          />
+        </div>
+
+        <div className="ml-auto">
+          <AvatarProfile />
         </div>
       </div>
 
-      <div className="flex justify-center items-center h-screen w-screen border-2">
-        <div className="container mx-auto">
-          <div className="flex justify-center items-center mb-5">
-            <div className="flex justify-center">
-              <img
-                src="https://phi-cargo.com/wp-content/uploads/2021/05/logo-phicargo-vertical.png"
-                height="400px"
-                width="400px"
-                alt="Logo Phi Cargo"
-                className="z-[200000000]"
-              />
-            </div>
-          </div>
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-5">
+      <div className="flex-grow flex items-center relative lg:mx-12 z-10 -mt-10 sm:-mt-14 md:-mt-20">
+        <div className="w-full">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-2">
             {filteredMenuItems.map((item, index) => (
               <MenuItem
                 key={index}
@@ -95,7 +135,7 @@ const MainMenuPage = () => {
           </div>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 
