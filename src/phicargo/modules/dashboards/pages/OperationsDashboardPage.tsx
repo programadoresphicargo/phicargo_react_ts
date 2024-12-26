f184aa70c4a169d7a75f5479c6b040125e6e83b9
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-12-26 09:22:49 -0600

    fix dashboard init

--- src/phicargo/modules/dashboards/pages/OperationsDashboardPage.tsx
+++ src/phicargo/modules/dashboards/pages/OperationsDashboardPage.tsx
@@ -58,6 +58,27 @@ const OperationsDashboardPage = () => {
     ],
   });
 
+  const onChangeData = () => {
+    setChartData({
+      labels: Data.map((data) => data.year),
+    datasets: [
+      {
+        label: 'Users Gained ',
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          'rgba(75,192,192,1)',
+          '#ecf0f1',
+          '#50AF95',
+          '#f3ba2f',
+          '#2a71d0',
+        ],
+        borderColor: 'black',
+        borderWidth: 2,
+      },
+    ],
+    })
+  }
+
   return (
     <div className="container">
       <h2 style={{ textAlign: 'center' }}>Pie Chart</h2>
@@ -72,6 +93,7 @@ const OperationsDashboardPage = () => {
           },
         }}
       />
+      <button onClick={onChangeData}>Change Data</button>
     </div>
   );
 };
