cbe5102aaff2ed2498466e8918e43ee2d80729e3
programadoresphicargo <desarrollador@phi-cargo.com>
2025-01-08 16:18:16 -0600

    4:18 pm

--- src/phicargo/reportes/llegadas_tarde/llegadas_tarde.jsx
+++ src/phicargo/reportes/llegadas_tarde/llegadas_tarde.jsx
@@ -3,15 +3,20 @@ import { MaterialReactTable, useMaterialReactTable } from 'material-react-table'
 import axios from 'axios';
 import Badge from 'react-bootstrap/Badge';
 import { mkConfig, generateCsv, download } from 'export-to-csv';
-import { Box, Button } from '@mui/material';
+import { Box } from '@mui/material';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 import { Component } from "react";
+import { Button, DatePicker } from '@nextui-org/react';
+import odooApi from '@/phicargo/modules/core/api/odoo-api';
+import ResponsiveAppBar from '@/phicargo/saldos_contabilidad/Navbar';
+import { parseDate, getLocalTimeZone } from "@internationalized/date";
+import { useDateFormatter } from "@react-aria/i18n";
 
 const DetencionesTable = () => {
 
   const [isLoading, setisLoading] = useState('');
-  const [fechaInicio, setFechaInicio] = useState('');
-  const [fechaFin, setFechaFin] = useState('');
+  const [fechaInicio, setFechaInicio] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
+  const [fechaFin, setFechaFin] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
 
   const handleFechaInicioChange = (event) => {
     setFechaInicio(event.target.value);
@@ -21,96 +26,37 @@ const DetencionesTable = () => {
     setFechaFin(event.target.value);
   };
 
-  const generarReporte = () => {
-    console.log('Fecha Inicio:', fechaInicio);
-    console.log('Fecha Fin:', fechaFin);
-    fetchData();
-  };
-
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fechaInicio, fechaFin]);
 
   const fetchData = async () => {
     try {
       setisLoading(true);
-      const response = await axios.get('/phicargo/reportes/llegadas_tarde/tabla1.php', {
-        params: {
-          fecha_inicio: fechaInicio,
-          fecha_fin: fechaFin
-        }
-      });
+      const response = await odooApi.get(`/tms_travel/salidas_llegadas/${fechaInicio}/${fechaFin}/`);
       setData(response.data);
       setisLoading(false);
-      console.log(response.data); // Maneja la respuesta aquí
+      console.log(response.data);
     } catch (error) {
       console.error('Error al enviar los datos:', error);
     }
   };
 
   const columns = [
-    {
-      accessorKey: 'referencia_anterior',
-      header: 'Viaje anterior / Finalización',
-      Cell: ({ row }) => (
-        `${row.original.referencia_anterior} - ${row.original.fecha_finalizado_anterior}`
-      ),
-    },
-    { accessorKey: 'sucursal', header: 'Sucursal' },
-    { accessorKey: 'referencia_actual', header: 'Referencia viaje' },
-    { accessorKey: 'estado_actual', header: 'Estado viaje' },
-    { accessorKey: 'nombre_operador_actual', header: 'Nombre operador' },
-    { accessorKey: 'unidad_actual', header: 'Unidad' },
-    { accessorKey: 'route_id_actual', header: 'Ruta' },
-    { accessorKey: 'fecha_programada_actual', header: 'Inicio de ruta programado' },
-    { accessorKey: 'fecha_inicio_actual', header: 'Inicio de ruta real' },
-    {
-      accessorKey: 'estado_salida',
-      header: 'Estado de inicio de ruta',
-      Cell: ({ row }) => {
-        const estado = row.original.estado_salida;
-
-        if (estado == 'Ya va tarde') {
-          return <Badge className="badge bg-danger">Ya va tarde</Badge>;
-        } else if (estado == 'Está en tiempo') {
-          return <Badge className="badge bg-primary">Está en tiempo</Badge>;
-        } else if (estado == 'Salió tarde') {
-          return <Badge className="badge bg-danger">Salió tarde</Badge>;
-        } else if (estado == 'Salió a tiempo') {
-          return <Badge className="badge bg-success">Salió a tiempo</Badge>;
-        }
-        return null;
-      },
-    },
-    { accessorKey: 'tiempo_diferencia_2_actual', header: 'Diferencia tiempo' },
-    { accessorKey: 'fecha_planta_actual', header: 'Llegada a planta programada' },
-    { accessorKey: 'fecha_envio_actual', header: 'Llegada a planta reportada' },
-    {
-      accessorKey: 'estado_llegada_planta',
-      header: 'Estado llegada a planta',
-      Cell: ({ row }) => {
-        const estado = row.original.estado_llegada_planta;
-
-        if (estado == 'Va a llegar tarde') {
-          return <Badge className="badge bg-warning">Va a llegar tarde</Badge>;
-        } else if (estado == 'Llego tarde') {
-          return <Badge className="badge bg-danger">Llego tarde</Badge>;
-        } else if (estado == 'Está en tiempo') {
-          return <Badge className="badge bg-primary">Está en tiempo</Badge>;
-        } else if (estado == 'Llego a tiempo') {
-          return <Badge className="badge bg-success">Llego a tiempo</Badge>;
-        }
-        return null;
-      },
-    },
-    { accessorKey: 'tiempo_diferencia_actual', header: 'Diferencia tiempo' },
-    { accessorKey: 'fecha_salida_planta', header: 'Salida planta' },
-    { accessorKey: 'detenciones_patio_planta', header: 'Minutos detenciones de patio a cliente' },
-    { accessorKey: 'detenciones_planta', header: 'Minutos detenido en planta' },
-    { accessorKey: 'detenciones_planta_patio', header: 'Minutos detenido planta a patio' },
+    { accessorKey: 'travel_name', header: 'Referencia' },
+    { accessorKey: 'route_name', header: 'Ruta' },
+    { accessorKey: 'employee_name', header: 'Operador' },
+    { accessorKey: 'x_ejecutivo_viaje_bel', header: 'Ejecutivo' },
+    { accessorKey: 'inicio_programado', header: 'Inicio programado' ,},
+    { accessorKey: 'fecha_inicio', header: 'Fecha inicio' },
+    { accessorKey: 'diferencia_tiempo', header: 'Diferencia tiempo salida' },
+    { accessorKey: 'llegada_planta_programada', header: 'Llegada a planta programada' },
+    { accessorKey: 'llegada_planta', header: 'Llegada a planta reportada' },
+    { accessorKey: 'diferencia_llegada_planta', header: 'Diferencia tiempo planta' },
+    { accessorKey: 'fecha_finalizado', header: 'Fecha finalizado' },
   ];
 
   const csvConfig = mkConfig({
@@ -134,93 +80,94 @@ const DetencionesTable = () => {
   const table = useMaterialReactTable({
     columns,
     data,
-    state: { isLoading: isLoading },
+    state: { showProgressBars: isLoading },
     enableRowSelection: true,
     columnFilterDisplayMode: 'popover',
     paginationDisplayMode: 'pages',
     positionToolbarAlertBanner: 'bottom',
     initialState: {
-      pagination: { pageSize: 80 }
+      density: 'compact',
+      expanded: false,
+      pagination: { pageIndex: 0, pageSize: 100 },
+      showColumnFilters: true,
     },
+    muiTableHeadCellProps: {
+      sx: {
+        fontFamily: 'Inter',
+        fontWeight: 'Bold',
+        fontSize: '14px',
+      },
+    },
+    muiTableContainerProps: {
+      sx: {
+        maxHeight: 'calc(100vh - 220px)',
+      },
+    },
+    muiTableBodyCellProps: ({ row }) => ({
+      sx: {
+        backgroundColor: row.subRows?.length ? '#0456cf' : '#FFFFFF',
+        fontFamily: 'Inter',
+        fontWeight: 'normal',
+        fontSize: '14px',
+        color: row.subRows?.length ? '#FFFFFF' : '#000000',
+      },
+    }),
     renderTopToolbarCustomActions: ({ table }) => (
       <Box
         sx={{
           display: 'flex',
           gap: '16px',
           padding: '8px',
-          flexWrap: 'wrap',
+          flexWrap: 'nowrap',
+          flexDirection: 'row',
+          alignItems: 'center',
         }}
       >
+        <DatePicker
+          variant='bordered'
+          label="Fecha inicio"
+          value={fechaInicio}
+          onChange={setFechaInicio}>
+        </DatePicker>
+
+        <DatePicker
+          variant='bordered'
+          label="Fecha fin"
+          value={fechaFin}
+          onChange={setFechaFin}>
+        </DatePicker>
+
         <Button
-          //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+          fullWidth={true}
+          color='primary'
           onClick={handleExportData}
-          startIcon={<FileDownloadIcon />}
+          startContent={<FileDownloadIcon />}
         >
-          Export All Data
+          Exportar todo
         </Button>
+
         <Button
-          disabled={table.getPrePaginationRowModel().rows.length === 0}
-          //export all rows, including from the next page, (still respects filtering and sorting)
-          onClick={() =>
-            handleExportRows(table.getPrePaginationRowModel().rows)
-          }
-          startIcon={<FileDownloadIcon />}
-        >
-          Export All Rows
-        </Button>
-        <Button
-          disabled={table.getRowModel().rows.length === 0}
-          //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
-          onClick={() => handleExportRows(table.getRowModel().rows)}
-          startIcon={<FileDownloadIcon />}
-        >
-          Export Page Rows
-        </Button>
-        <Button
-          disabled={
+          fullWidth={true}
+          color='primary'
+          isDisabled={
             !table.getIsSomeRowsSelected() && !table.getIsAllRowsSelected()
           }
-          //only export selected rows
           onClick={() => handleExportRows(table.getSelectedRowModel().rows)}
-          startIcon={<FileDownloadIcon />}
+          startContent={<FileDownloadIcon />}
         >
-          Export Selected Rows
+          Exportar seleccionado
         </Button>
       </Box >
     ),
   });
 
-  return (<div>
-    <div className='row'>
-      <div className='col-2'>
-        <input
-          className='form-control'
-          id="fechaInicio"
-          type="date"
-          onChange={handleFechaInicioChange}
-        />
-      </div>
-      <div className='col-2'>
-        <input
-          className='form-control'
-          id="fechaFin"
-          type="date"
-          onChange={handleFechaFinChange}
-        />
-      </div>
-      <div className='col-3'>
-        <button
-          className="btn btn-primary"
-          onClick={generarReporte}
-        >
-          Generar reporte
-        </button>
-      </div>
-    </div>
-    <MaterialReactTable
-      table={table}
-    />;
-  </div>
+  return (
+    <>
+      <ResponsiveAppBar></ResponsiveAppBar>
+      <MaterialReactTable
+        table={table}
+      />;
+    </>
   );
 };
 
