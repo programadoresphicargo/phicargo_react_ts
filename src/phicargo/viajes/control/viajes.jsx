d850eeeaf0656d7b3c15706cc941554c3af62721
programadoresphicargo <desarrollador@phi-cargo.com>
2025-01-13 09:38:51 -0600

    9:38 am

--- src/phicargo/viajes/control/viajes.jsx
+++ src/phicargo/viajes/control/viajes.jsx
@@ -319,6 +319,7 @@ const ViajesActivos = ({ }) => {
       >
         <h1 className='text-primary'>Gestión de viajes</h1>
         <Button className='text-white' color='success'
+          isDisabled={true}
           onClick={() => setMasivoOpen(true)}
         >Envio masivo</Button>
       </Box>
