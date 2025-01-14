783b214b75f5cdb6a11056b19903a0f934facc1b
programadoresphicargo <desarrollador@phi-cargo.com>
2025-01-14 14:10:27 -0600

    2:10 pm

--- src/phicargo/maniobras/maniobras/cancelar_maniobra.jsx
+++ src/phicargo/maniobras/maniobras/cancelar_maniobra.jsx
@@ -4,38 +4,32 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
+import { Button } from "@nextui-org/react"
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useAuthContext } from '@/phicargo/modules/auth/hooks';
-const { VITE_PHIDES_API_URL } = import.meta.env;
+import odooApi from '@/phicargo/modules/core/api/odoo-api';
 
 const CancelarManiobraDialog = ({ open, handleClose, id_maniobra }) => {
-    const { session } = useAuthContext();
     const [loading, setLoading] = useState(false);
 
     const cancelarManiobra = () => {
 
-        const postData = new URLSearchParams();
-        postData.append('id_maniobra', id_maniobra);
-        postData.append('id_usuario', session.user.id);
+        setLoading(true);
 
-        setLoading(true); // Mostrar un estado de carga mientras se realiza la solicitud
-
-        axios.post(VITE_PHIDES_API_URL + `/modulo_maniobras/maniobra/cancelar_maniobra.php`, postData)
+        odooApi.get(`/maneuvers/cancelar_maniobra/` + id_maniobra)
             .then((response) => {
                 setLoading(false);
-                if (response.data === 1) {
-                    handleClose(); // Cerrar el diálogo
-                    toast.success('El registro ha sido cancelado exitosamente.');
+                if (response.data.status === 'success') {
+                    handleClose();
+                    toast.success(response.data.message);
                 } else {
-                    console.error(response.data);
-                    toast.error('Hubo un error al intentar cancelar la maniobra.');
+                    toast.error(response.data.message);
                 }
             })
             .catch((error) => {
                 setLoading(false);
-                toast.error('Error: Hubo un problema al eliminar el registro.');
+                toast.error('Error: Hubo un problema al cancelar el registro.' + error);
             });
     };
 
@@ -60,7 +54,7 @@ const CancelarManiobraDialog = ({ open, handleClose, id_maniobra }) => {
                     onClick={cancelarManiobra}
                     color="secondary"
                     autoFocus
-                    disabled={loading} // Desactivar el botón mientras se procesa
+                    disabled={loading}
                 >
                     Sí, cancelar
                 </Button>
