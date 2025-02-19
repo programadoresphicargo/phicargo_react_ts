a21272e89f4bc284f18c044dce2c3ce37f5723d9
programadoresphicargo <desarrollador@phi-cargo.com>
2025-02-19 11:58:08 -0600

    11:58 am

--- src/phicargo/bonos/BonosMes.jsx
+++ src/phicargo/bonos/BonosMes.jsx
@@ -27,7 +27,6 @@ const BonosMes = ({ month, year }) => {
 
   const fetchPermisos = async () => {
     try {
-      toast.info('Obteniendo permisos');
       const response = await odooApi.get(`/users-management/permissions/` + session.user.id);
       const ids = response.data.map(permiso => permiso.permission_id);
       setPermisos(ids);
