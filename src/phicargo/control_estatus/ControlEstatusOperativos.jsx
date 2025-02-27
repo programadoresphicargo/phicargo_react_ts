aa538473474364e29acb4a4b70dd569959bcc108
programadoresphicargo <desarrollador@phi-cargo.com>
2025-02-27 15:18:00 -0600

    3:17 pm

--- src/phicargo/control_estatus/ControlEstatusOperativos.jsx
+++ src/phicargo/control_estatus/ControlEstatusOperativos.jsx
@@ -3,13 +3,16 @@ import { Tabs, Tab, Card, CardBody } from "@heroui/react";
 import Maniobras from "./tabla";
 import Typography from '@mui/material/Typography';
 import NavbarViajes from "../viajes/navbar";
+import { ViajeProvider } from "../viajes/context/viajeContext";
 
 export default function ControlEstatusOperativos() {
     return (
         <div>
-            <NavbarViajes></NavbarViajes>
-            <Maniobras
-                estado_maniobra={'activo'} />
+            <ViajeProvider>
+                <NavbarViajes></NavbarViajes>
+                <Maniobras
+                    estado_maniobra={'activo'} />
+            </ViajeProvider>
         </div>
     );
 }
