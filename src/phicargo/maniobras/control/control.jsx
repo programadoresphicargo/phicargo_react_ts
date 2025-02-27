caac69bd47b847388ea6ff65f815a396c1791307
programadoresphicargo <desarrollador@phi-cargo.com>
2025-02-27 15:50:20 -0600

    3:50 pm

--- src/phicargo/maniobras/control/control.jsx
+++ src/phicargo/maniobras/control/control.jsx
@@ -1,32 +1,47 @@
 import React from "react";
-import { Tabs, Tab, Card, CardBody } from "@heroui/react";
 import Maniobras from "./tabla";
 import ManiobrasNavBar from "../Navbar";
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
 export default function control_maniobras() {
+
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
+        <>
             <ManiobrasNavBar />
-            <div className="m-1">
-                <Tabs aria-label="Options" color={'primary'} size="lg">
-                    <Tab key="photos" title="Activas">
-                        <Maniobras
-                            estado_maniobra={'activa'} />
-                    </Tab>
-                    <Tab key="music" title="Programadas">
-                        <Maniobras
-                            estado_maniobra={'borrador'} />
-                    </Tab>
-                    <Tab key="videos" title="Finalizadas">
-                        <Maniobras
-                            estado_maniobra={'finalizada'} />
-                    </Tab>
-                    <Tab key="mp" title="Pendientes por asignar">
-                        <Maniobras
-                            estado_maniobra={'mp'} />
-                    </Tab>
-                </Tabs>
-            </div>
-        </div>
+            <Box sx={{ width: '100%' }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderColor: 'divider', backgroundColor: '#002887', color: 'white' }}>
+                        <TabList onChange={handleChange}
+                            textColor="inherit"
+                            sx={{
+                                '& .MuiTabs-indicator': {
+                                    backgroundColor: 'white', 
+                                    height: '3px',
+                                }
+                            }}
+                        >
+                            <Tab label="ACTIVAS" value="1" sx={{ fontFamily: 'Inter' }} />
+                            <Tab label="BORRADOR" value="2" sx={{ fontFamily: 'Inter' }} />
+                            <Tab label="FINALIZADAS" value="3" sx={{ fontFamily: 'Inter' }} />
+                            <Tab label="PENDIENTES POR ASIGNAR" value="4" sx={{ fontFamily: 'Inter' }} />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1" sx={{ padding: 0, margin: 0 }}><Maniobras estado_maniobra={'activa'} /></TabPanel>
+                    <TabPanel value="2" sx={{ padding: 0, margin: 0 }}><Maniobras estado_maniobra={'borrador'} /></TabPanel>
+                    <TabPanel value="3" sx={{ padding: 0, margin: 0 }}> <Maniobras estado_maniobra={'finalizada'} /></TabPanel>
+                    <TabPanel value="4" sx={{ padding: 0, margin: 0 }}><Maniobras estado_maniobra={'mp'} /></TabPanel>
+                </TabContext>
+            </Box>
+        </>
     );
 }
