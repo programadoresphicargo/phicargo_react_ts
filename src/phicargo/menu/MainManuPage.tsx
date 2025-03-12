fa8479c45ca642764187a86d256416026f4a82b8
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-03-11 18:02:40 -0600

    few changes

--- src/phicargo/menu/MainManuPage.tsx
+++ src/phicargo/menu/MainManuPage.tsx
@@ -181,7 +181,7 @@ const MainMenuPage = () => {
         </Toolbar>
       </AppBar> */}
 
-      <div
+      <main
         style={{
           backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 400"><rect width="1920" height="400" fill="%23D9DEEA" /><mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="1920" height="400"><rect width="1920" height="400" fill="%23D9DEEA" /></mask><g mask="url(%23mask0)"><path d="M1059.48 308.024C1152.75 57.0319 927.003 -103.239 802.47 -152.001L1805.22 -495.637L2095.53 351.501L1321.23 616.846C1195.12 618.485 966.213 559.015 1059.48 308.024Z" fill="%23C0CBDD" /><path d="M1333.22 220.032C1468.66 -144.445 1140.84 -377.182 960 -447.991L2416.14 -947L2837.71 283.168L1713.32 668.487C1530.19 670.868 1197.78 584.509 1333.22 220.032Z" fill="%238192B0" /></g></svg>')`,
           backgroundSize: 'cover',
@@ -216,7 +216,6 @@ const MainMenuPage = () => {
             />
           </div>
         </div>
-        <div className="mb-8" />{' '}
         <div className="flex-grow flex items-center relative lg:mx-12 z-10 -mt-10 sm:-mt-14 md:-mt-20">
           <div className="w-full">
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-7 gap-2">
@@ -232,7 +231,7 @@ const MainMenuPage = () => {
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 };
