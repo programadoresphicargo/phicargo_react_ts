5cb3f97078f825838a7a98e132d41d784f35ae8f
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-19 16:42:35 -0600

    4:42 pm

---
+++ src/phicargo/politica/politica.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const PoliticaUso = () => {
+    return (
+        <div className="max-w-3xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+            <h1 className="text-2xl font-bold mb-4">Política de Uso del Sitio</h1>
+            <p className="mb-4">
+                Bienvenido a nuestro sitio web. Al acceder y utilizar este sitio, usted acepta cumplir con los siguientes términos y condiciones de uso.
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">1. Uso Aceptable</h2>
+            <p className="mb-4">
+                Usted se compromete a utilizar este sitio de manera responsable y legal, sin incurrir en actividades que puedan dañar la integridad del mismo.
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">2. Propiedad Intelectual</h2>
+            <p className="mb-4">
+                Todo el contenido presente en este sitio es propiedad de la empresa y está protegido por leyes de derechos de autor y propiedad intelectual.
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">3. Privacidad</h2>
+            <p className="mb-4">
+                Nos comprometemos a proteger su privacidad y manejar su información personal de acuerdo con nuestra política de privacidad.
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">4. Modificaciones</h2>
+            <p className="mb-4">
+                Nos reservamos el derecho de modificar estos términos en cualquier momento sin previo aviso.
+            </p>
+
+            <h2 className="text-xl font-semibold mt-4">5. Contacto</h2>
+            <p>
+                Si tiene alguna pregunta sobre nuestra política de uso, puede contactarnos a través de nuestro formulario de contacto.
+            </p>
+        </div>
+    );
+};
+
+export default PoliticaUso;
--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import LoginPage from '../phicargo/modules/auth/pages/LoginPage';
 import { useAuthContext } from '@/phicargo/modules/auth/hooks';
 import { useEffect } from 'react';
+import PoliticaUso from '@/phicargo/politica/politica';
 
 export const PublicRoutes = () => {
   const location = useLocation();
@@ -17,8 +18,9 @@ export const PublicRoutes = () => {
 
   return (
     <Routes>
-      <Route path="*" element={<Navigate to={'/auth/login'} />} />
       <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/politicas" element={<PoliticaUso />} />
+      <Route path="*" element={<Navigate to={'/auth/login'} />} />
     </Routes>
   );
 };
