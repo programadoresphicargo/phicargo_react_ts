464fafea6ae54d7e2bbd26ae8a762efa05a6fa3c
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-19 16:45:29 -0600

    4:45 pm

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -3,7 +3,6 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import LoginPage from '../phicargo/modules/auth/pages/LoginPage';
 import { useAuthContext } from '@/phicargo/modules/auth/hooks';
 import { useEffect } from 'react';
-import PoliticaUso from '@/phicargo/politica/politica';
 
 export const PublicRoutes = () => {
   const location = useLocation();
@@ -18,9 +17,8 @@ export const PublicRoutes = () => {
 
   return (
     <Routes>
-      <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/politicas" element={<PoliticaUso />} />
       <Route path="*" element={<Navigate to={'/auth/login'} />} />
+      <Route path="/auth/login" element={<LoginPage />} />
     </Routes>
   );
 };
