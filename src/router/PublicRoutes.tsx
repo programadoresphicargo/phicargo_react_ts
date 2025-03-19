e9d0b13168afc1c640bd816ee3d40ea77bc10ada
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-19 16:49:12 -0600

    4:49 pm

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -1,24 +1,27 @@
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 import LoginPage from '../phicargo/modules/auth/pages/LoginPage';
 import { useAuthContext } from '@/phicargo/modules/auth/hooks';
-import { useEffect } from 'react';
+import PoliticaUso from '@/phicargo/politica/politica';
 
 export const PublicRoutes = () => {
   const location = useLocation();
   const { setRedirectTo } = useAuthContext();
 
+  const publicPaths = ['/auth/login', '/politicas'];
+
   useEffect(() => {
-    if (location.pathname !== '/auth/login') {
+    if (!publicPaths.includes(location.pathname)) {
       setRedirectTo(location.pathname);
     }
   }, [location, setRedirectTo]);
 
-
   return (
     <Routes>
-      <Route path="*" element={<Navigate to={'/auth/login'} />} />
       <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/politicas" element={<PoliticaUso />} />
+      <Route path="*" element={<Navigate to={'/auth/login'} />} />
     </Routes>
   );
 };
