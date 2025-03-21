a1627e667ddeb41b06c32af6db9085b801d0e53c
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-21 13:44:01 -0600

    1:43 pm

--- src/phicargo/modules/auth/hooks/useLoginMutation.ts
+++ src/phicargo/modules/auth/hooks/useLoginMutation.ts
@@ -1,11 +1,8 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import AuthServiceApi from '../services/auth-service';
-import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useAuthContext } from './useAuthContext';
-
-const { VITE_PHIDES_API_URL } = import.meta.env;
 
 /**
  * Hook para realizar la mutación de login
@@ -15,26 +12,6 @@ export const useLoginMutation = () => {
   const queryClient = useQueryClient();
 
   const { setAuthStatus, setSession } = useAuthContext();
-
-  const startSession = async (userID: number) => {
-    try {
-      const response = await axios.post(
-        `${VITE_PHIDES_API_URL}/login/inicio/start_session.php`,
-        { userID: userID }, // Envía el userID en el cuerpo
-        { withCredentials: true } // Habilita cookies y credenciales
-      );
-
-      if (response.data.success) {
-        toast.success("Éxito: " + response.data.message);
-      } else {
-        toast.error("Error lógico: " + response.data.message);
-      }
-    } catch (error) {
-      console.error("Error al iniciar la sesión:", error);
-      toast.error("Error al comunicarse con el servidor.");
-    }
-  };
-
 
   const loginMutation = useMutation({
     mutationFn: AuthServiceApi.login,
@@ -46,7 +23,6 @@ export const useLoginMutation = () => {
       setAuthStatus('authenticated');
       setSession(session);
       sessionStorage.setItem('session', JSON.stringify(session));
-      startSession(session.user.id);
     },
     onError: (error: Error) => {
       setAuthStatus('unauthenticated');
