f35c26f9718274045ca8a69184b7a975e1cbd2e4
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-03-24 17:35:56 -0600

    update pilicies page

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -1,15 +1,17 @@
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 
+import { LoadingPage } from '@/pages/LoadingPage';
 import LoginPage from '../phicargo/modules/auth/pages/LoginPage';
 import { useAuthContext } from '@/phicargo/modules/auth/hooks';
-import PoliticaUso from '@/phicargo/politica/politica';
+
+const UsagePoliciesPage = lazy(() => import('@/pages/UsagePoliciesPage'));
+
+const publicPaths = ['/auth/login', '/politicas'];
 
 export const PublicRoutes = () => {
   const location = useLocation();
   const { setRedirectTo } = useAuthContext();
-
-  const publicPaths = ['/auth/login', '/politicas'];
 
   useEffect(() => {
     if (!publicPaths.includes(location.pathname)) {
@@ -20,8 +22,16 @@ export const PublicRoutes = () => {
   return (
     <Routes>
       <Route path="/auth/login" element={<LoginPage />} />
-      <Route path="/politicas" element={<PoliticaUso />} />
+      <Route
+        path="/politicas"
+        element={
+          <Suspense fallback={<LoadingPage />}>
+            <UsagePoliciesPage />
+          </Suspense>
+        }
+      />
       <Route path="*" element={<Navigate to={'/auth/login'} />} />
     </Routes>
   );
 };
+
