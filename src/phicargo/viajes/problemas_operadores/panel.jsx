b829a847fc113d35e183cd7af41556c6c30df6d2
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-25 10:56:41 -0600

    10:56 am

--- src/phicargo/viajes/problemas_operadores/panel.jsx
+++ src/phicargo/viajes/problemas_operadores/panel.jsx
@@ -1,46 +1,33 @@
-import { Avatar, AvatarGroup, AvatarIcon, Button, Card, CardBody, CardFooter, CardHeader } from "@heroui/react"
-import React, { useEffect, useState, } from 'react';
+import { Avatar, Card, CardBody, CardFooter, CardHeader, CircularProgress } from "@heroui/react";
+import {
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerContent,
+    DrawerFooter,
+    DrawerHeader,
+    useDisclosure,
+} from "@heroui/react";
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import { CircularProgress } from "@heroui/react";
 import { Container } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ReporteOperador from './reporte';
 import Toolbar from '@mui/material/Toolbar';
+import Travel from '../control/viaje';
 import Typography from '@mui/material/Typography';
-import odooApi from "@/phicargo/modules/core/api/odoo-api";
-import {
-    Drawer,
-    DrawerContent,
-    DrawerHeader,
-    DrawerBody,
-    DrawerFooter,
-    useDisclosure,
-} from "@heroui/react";
-export default function ProblemasOperadores({ isOpen, onOpen, onOpenChange }) {
+import { ViajeContext } from '../context/viajeContext';
+import odooApi from '@/phicargo/modules/core/api/odoo-api';
+import { tiempoTranscurrido } from '../../funciones/tiempo';
+import ReporteOperador from "./reporte";
+
+export default function ProblemasOperadores2({ isOpen, onOpen, onOpenChange }) {
 
     const [id_reporte, setIDReporte] = React.useState("");
     const [estatus, setEstatus] = React.useState([]);
     const [isLoading, setLoading] = React.useState(false);
-
-    useEffect(() => {
-        getEstatus();
-    }, [isOpen]);
-
-    const getEstatus = async () => {
-        try {
-            setLoading(true);
-            const response = await odooApi.get('/problemas_operadores/');
-            setEstatus(response.data);
-        } catch (error) {
-            console.error('Error al obtener los datos:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
 
     const [openReporte, setOpenReporte] = React.useState(false);
 
@@ -53,44 +40,89 @@ export default function ProblemasOperadores({ isOpen, onOpen, onOpenChange }) {
         setOpenReporte(false);
     };
 
+    useEffect(() => {
+        getEstatus();
+    }, [open]);
+
+    const getEstatus = async () => {
+
+        try {
+            setLoading(true);
+            const response = await odooApi.get('/problemas_operadores/');
+            console.log(response.data);
+            setEstatus(response.data);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.error('Error al obtener los datos:', error);
+        }
+    };
+
     return (
-        <div>
-            <Drawer isOpen={isOpen} size={"lg"} onOpenChange={onOpenChange}
+        <>
+            <ReporteOperador id_reporte={id_reporte} isOpen={openReporte} onOpenChange={handleCloseReporte}></ReporteOperador>
+            <Drawer
+                isOpen={isOpen}
+                size='lg'
+                motionProps={{
+                    variants: {
+                        enter: {
+                            opacity: 1,
+                            x: 0,
+                            duration: 0.3,
+                        },
+                        exit: {
+                            x: 100,
+                            opacity: 0,
+                            duration: 0.3,
+                        },
+                    },
+                }}
+                onOpenChange={onOpenChange}
             >
                 <DrawerContent>
                     {(onClose) => (
                         <>
-                            <DrawerHeader className="flex flex-col gap-1">Problemas del operador</DrawerHeader>
+                            <DrawerHeader className="flex flex-col gap-1">Problemas operador</DrawerHeader>
                             <DrawerBody>
 
-                                {isLoading && (
-                                    <div style={{ marginTop: '20px' }} className="d-flex justify-content-center">
-                                        <CircularProgress size="lg" aria-label="Loading..." color='danger' />
-                                    </div>
-                                )}
+                                <ul class="list-group list-group-flush navbar-card-list-group">
 
-                                {estatus.map((step) => (
-                                    <Card key={step.id_reporte} className="m-1" onPress={() => handleClickOpen(step.id_reporte)}>
-                                        <CardBody className="px-3 text-small text-default-400">
-                                            <div className="flex gap-5">
-                                                <Avatar color='danger' isBordered radius="full" size="md" />
-                                                <div className="flex flex-col gap-1 items-start justify-center">
-                                                    <h4 className="text-small font-semibold leading-none text-default-600">{step.referencia}</h4>
-                                                    <h5 className="text-small tracking-tight text-default-400">{step.nombre_operador}</h5>
-                                                </div>
-                                            </div>
-                                        </CardBody>
-                                        <CardFooter className="gap-3">
-                                            <div className="flex gap-1">
-                                                <p className=" text-default-400 text-small">{step.fecha_creacion}</p>
-                                            </div>
-                                        </CardFooter>
-                                    </Card>
-                                ))}
+                                    {isLoading && (
+                                        <div style={{ marginTop: '20px' }} className="d-flex justify-content-center">
+                                            <CircularProgress size="lg" aria-label="Loading..." />
+                                        </div>
+                                    )}
 
+                                    {estatus.map((step, index) => (
+                                        <>
+                                            <Card className='m-2' onPress={() => handleClickOpen(step.id_reporte)} isPressable fullWidth>
+                                                <CardHeader className="justify-between">
+                                                    <div className="flex gap-5">
+                                                        <Avatar
+                                                            color='danger'
+                                                            isBordered
+                                                            radius="full"
+                                                            size="md"
+                                                        />
+                                                        <div className="flex flex-col gap-1 items-start justify-center">
+                                                            <h4 className="text-small font-semibold leading-none text-default-600">{step.referencia}</h4>
+                                                            <h5 className="text-small tracking-tight text-default-400">{step.nombre_operador}</h5>
+                                                        </div>
+                                                    </div>
+                                                </CardHeader>
+                                                <CardFooter className="gap-3">
+                                                    <div className="flex gap-1">
+                                                        <p className="font-semibold text-default-400 text-small">{step.fecha_creacion}</p>
+                                                    </div>
+                                                </CardFooter>
+                                            </Card>
+                                        </>
+                                    ))}
+                                </ul>
                             </DrawerBody>
                             <DrawerFooter>
-                                <Button color="danger" variant="light" onPress={onOpenChange}>
+                                <Button color="primary" onPress={onClose}>
                                     Cerrar
                                 </Button>
                             </DrawerFooter>
@@ -98,9 +130,6 @@ export default function ProblemasOperadores({ isOpen, onOpen, onOpenChange }) {
                     )}
                 </DrawerContent>
             </Drawer>
-
-            <ReporteOperador id_reporte={id_reporte} isOpen={openReporte} onOpenChange={handleCloseReporte}></ReporteOperador>
-
-        </div>
+        </>
     );
 }
