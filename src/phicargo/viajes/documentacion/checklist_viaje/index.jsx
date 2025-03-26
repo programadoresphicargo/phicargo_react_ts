ea9440499586a354d73281c26e2cb441dde97070
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-26 16:31:16 -0600

    4:31 pm

--- src/phicargo/viajes/documentacion/checklist_viaje/index.jsx
+++ src/phicargo/viajes/documentacion/checklist_viaje/index.jsx
@@ -3,13 +3,15 @@ import axios from "axios";
 import { Button, Link } from "@heroui/react";
 import { generatePDF } from "./generatePDF";
 import odooApi from "@/phicargo/modules/core/api/odoo-api";
+import { ViajeContext } from "../../context/viajeContext";
 
 const PDFGenerator = () => {
+    const { id_viaje, viaje, getViaje, loading, error, setIDViaje, isLoading } = useContext(ViajeContext);
     const [data, setData] = useState([]);
     const [data_contenedores, setDataContenedores] = useState([]);
 
     useEffect(() => {
-        odooApi.get("/tms_travel/checklist/checklist_viajes_equipos/id_viaje/70692")
+        odooApi.get("/tms_travel/checklist/checklist_viajes_equipos/id_viaje/" + id_viaje)
             .then((response) => {
                 setData(response.data);
             })
@@ -17,7 +19,7 @@ const PDFGenerator = () => {
     }, []);
 
     useEffect(() => {
-        odooApi.get("/tms_travel/checklist/checklist_viajes_contenedores/id_viaje/70692")
+        odooApi.get("/tms_travel/checklist/checklist_viajes_contenedores/id_viaje/" + id_viaje)
             .then((response) => {
                 setDataContenedores(response.data);
             })
