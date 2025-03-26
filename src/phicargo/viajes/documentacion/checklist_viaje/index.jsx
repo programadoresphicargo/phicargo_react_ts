1e82fe19ae713b13f4ec831d8f6069f8d3183a83
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-26 16:34:13 -0600

    4:34 pm

--- src/phicargo/viajes/documentacion/checklist_viaje/index.jsx
+++ src/phicargo/viajes/documentacion/checklist_viaje/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { Button, Link } from "@heroui/react";
 import { generatePDF } from "./generatePDF";
