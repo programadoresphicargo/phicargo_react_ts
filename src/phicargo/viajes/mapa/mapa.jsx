fc594175f67e773dd208da74a28b7f07f736e31d
programadoresphicargo <desarrollador@phi-cargo.com>
2025-03-31 12:43:19 -0600

    12.43 pm

--- src/phicargo/viajes/mapa/mapa.jsx
+++ src/phicargo/viajes/mapa/mapa.jsx
@@ -40,9 +40,9 @@ const Map = () => {
     return (
         <MapContainer center={[21.9713317720013, -101.7129111380927]} zoom={5} style={{ height: '100vh', width: '100%' }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {estatus?.map((status, index) => (
-                <Marker key={index} position={[status.latitud, status.longitud]} icon={customIcon}>
-                    <Popup>{status.nombre_estatus}</Popup>
+            {estatus?.map((registro, index) => (
+                <Marker key={index} position={[registro.latitud, registro.longitud]} icon={customIcon}>
+                    <Popup>{registro.nombre_estatus}</Popup>
                 </Marker>
             ))}
             <Polyline positions={positions} color="blue" />
