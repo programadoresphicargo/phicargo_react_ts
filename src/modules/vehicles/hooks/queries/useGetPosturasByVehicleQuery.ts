908eb7bd1bc17f2067b9338805f75864d2270550
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-04-08 17:43:37 -0600

    Add useGetPosturasByVehicleQuery hook for fetching vehicle posturas

---
+++ src/modules/vehicles/hooks/queries/useGetPosturasByVehicleQuery.ts
@@ -0,0 +1,17 @@
+import type { Postura } from '../../models';
+import { PosturasService } from '../../services';
+import { useQuery } from '@tanstack/react-query';
+
+const POSTURAS_VEHICLE_KEY = 'posturas-vehicle';
+
+export const useGetPosturasByVehicleQuery = (vehicleId: number) => {
+  const getPosturasByVehicleQuery = useQuery<Postura[]>({
+    queryKey: [POSTURAS_VEHICLE_KEY, vehicleId],
+    queryFn: () => PosturasService.getPosturasByVehicle(vehicleId),
+  });
+
+  return {
+    getPosturasByVehicleQuery,
+  };
+};
+
