5ecc04da9e85c599276a869c4d455597d289276e
programadoresphicargo <desarrollador@phi-cargo.com>
2025-04-12 11:42:36 -0600

    11:42 am

--- src/phicargo/maniobras/maniobras/cancelar_maniobra.jsx
+++ src/phicargo/maniobras/maniobras/cancelar_maniobra.jsx
@@ -10,20 +10,32 @@ import axios from 'axios';
 import odooApi from '@/api/odoo-api';
 import { toast } from 'react-toastify';
 import { useAuthContext } from "@/modules/auth/hooks";
+import { Select, SelectItem } from "@heroui/react";
 
 const CancelarManiobraDialog = ({ open, handleClose, id_maniobra }) => {
     const [loading, setLoading] = useState(false);
+    const [motivo_cancelacion, setValue] = React.useState("");
+
+    const handleSelectionChange = (e) => {
+        setValue(e.target.value);
+    };
 
     const cancelarManiobra = () => {
 
+        if (motivo_cancelacion == "") {
+            toast.error('Añade un motivo de cancelación.');
+            return;
+        }
+
         setLoading(true);
 
-        odooApi.get(`/maniobras/cancelar/` + id_maniobra)
+        odooApi.get(`/maniobras/cancelar/${id_maniobra}/${motivo_cancelacion}`)
             .then((response) => {
                 setLoading(false);
                 if (response.data.status === 'success') {
                     handleClose();
                     toast.success(response.data.message);
+                    setValue("");
                 } else {
                     toast.error(response.data.message);
                 }
@@ -40,20 +52,32 @@ const CancelarManiobraDialog = ({ open, handleClose, id_maniobra }) => {
             onClose={handleClose}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
+            maxWidth
+
         >
             <DialogTitle id="alert-dialog-title">{"¿Estás seguro?"}</DialogTitle>
             <DialogContent>
                 <DialogContentText id="alert-dialog-description">
                     ¿Deseas realmente cancelar este registro?
                 </DialogContentText>
+
+                <Select
+                    className="max-w-xs mt-3"
+                    label="Motivo de cancelacion"
+                    onChange={handleSelectionChange}
+                >
+                    <SelectItem key="perdida_operador">Pérdida del operador</SelectItem>
+                    <SelectItem key="error_datos">Error en los datos</SelectItem>
+                </Select>
+
             </DialogContent>
             <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                    Cancelar
+                <Button onPress={handleClose} color="primary">
+                    Volver
                 </Button>
                 <Button
-                    onClick={cancelarManiobra}
-                    color="secondary"
+                    onPress={cancelarManiobra}
+                    color="danger"
                     autoFocus
                     disabled={loading}
                 >
