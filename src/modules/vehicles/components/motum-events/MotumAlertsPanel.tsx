ea63807ecd677e2bf0cfc140fd1d96028cb13a2d
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-04-15 15:50:04 -0600

    Refactor MotumAlertsPanel to optimize configuration logic using useMemo and improve date handling

--- src/modules/vehicles/components/motum-events/MotumAlertsPanel.tsx
+++ src/modules/vehicles/components/motum-events/MotumAlertsPanel.tsx
@@ -1,26 +1,28 @@
 import { Badge, IconButton, Tab, Tabs } from '@mui/material';
+import { useMemo, useState } from 'react';
 
 import Drawer from '@mui/material/Drawer';
 import type { MotumEventStatus } from '../../models';
 import { MotumEventsHistoryList } from './MotumEventsHistoryList';
 import { MotumEventsList } from './MotumEventsList';
 import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import dayjs from 'dayjs';
 import { useMotumEventsQueries } from '../../hooks/queries';
-import { useState } from 'react';
 
 export const MotumAlertsPanel = () => {
   const [open, setOpen] = useState(false);
   const [tab, setTab] = useState<MotumEventStatus>('pending');
   const [dateRagen, setDateRange] = useState<[string, string] | null>(null);
 
-  const conf =
-    tab === 'pending'
-      ? { status: tab }
-      : {
-          status: tab,
-          startDate: dateRagen?.[0],
-          endDate: dateRagen?.[1],
-        };
+  const conf = useMemo(() => {
+    const pendingStartDate = dayjs().subtract(2, 'day').format('YYYY-MM-DD');
+    const pendingEndDate = dayjs().format('YYYY-MM-DD');
+    return {
+      status: tab,
+      startDate: tab === 'attended' ? dateRagen?.[0] : pendingStartDate,
+      endDate: tab === 'attended' ? dateRagen?.[1] : pendingEndDate,
+    };
+  }, [tab, dateRagen]);
 
   const { getMotumEventsQuery } = useMotumEventsQueries(conf);
 
