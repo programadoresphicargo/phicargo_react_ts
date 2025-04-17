e30558f8f01184369c79f4a0f82304e567f10f76
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-04-17 13:53:19 -0600

    Refactor ComplaintsPage header and update indicator cards for better clarity

--- src/modules/complaints/components/ui/Header.tsx
+++ src/modules/complaints/components/ui/Header.tsx
@@ -8,15 +8,12 @@ const Header = () => {
         <h1 className="m-0 p-0 text-xl text-gray-100 font-bold">
           Quejas De Clientes
         </h1>
-        <p className="my-1 text-sm text-gray-300">
-          Información sobre el estado de los tractos en mantenimiento.
-        </p>
       </div>
 
       <div className="flex gap-2 flex-1">
-        <IndicatorCard title="Veracruz" content={0} isLoading={false} />
-        <IndicatorCard title="Manzanillo" content={0} isLoading={false} />
-        <IndicatorCard title="México" content={0} isLoading={false} />
+        <IndicatorCard title="Abiertas" content={0} isLoading={false} />
+        <IndicatorCard title="Cerradas" content={0} isLoading={false} />
+        <IndicatorCard title="En Proceso" content={0} isLoading={false} />
       </div>
     </HeaderBase>
   );
--- src/modules/complaints/pages/ComplaintsPage.tsx
+++ src/modules/complaints/pages/ComplaintsPage.tsx
@@ -1,5 +1,54 @@
+import { AddButton } from '@/components/ui';
+import type { Complaint } from '../models';
+import { MaterialReactTable } from 'material-react-table';
+import { useBaseTable } from '@/hooks';
+import { useComplaintsColumns } from '../hooks';
+import { useGetComplaintsQuery } from '../hooks/queries';
+
 const ComplaintsPage = () => {
-  return <div>ComplaintsPage</div>;
+  const {
+    getComplaintsQuery: { data, isLoading, isFetching, refetch },
+  } = useGetComplaintsQuery();
+
+  const columns = useComplaintsColumns();
+
+  const table = useBaseTable<Complaint>({
+    columns,
+    data: data || [],
+    isFetching,
+    isLoading,
+    refetchFn: refetch,
+    tableId: 'complaints-table',
+    containerHeight: 'calc(100vh - 220px)',
+    enableRowActions: true,
+    columnFilterDisplayMode: 'popover',
+    positionActionsColumn: 'first',
+    toolbarActions: (
+      <AddButton
+        label="Crear Queja"
+        size="small"
+        onClick={() => table.setCreatingRow(true)}
+      />
+    ),
+    // muiEditRowDialogProps: dialogProps,
+    // muiCreateRowModalProps: dialogProps,
+    // renderEditRowDialogContent: ({ table, row }) => (
+    //   <EditRecordForm
+    //     onClose={() => table.setEditingRow(null)}
+    //     record={row.original}
+    //   />
+    // ),
+    // renderCreateRowDialogContent: ({ table }) => (
+    //   <CreateRecordForm onClose={() => table.setCreatingRow(null)} />
+    // ),
+  });
+
+  return (
+    <>
+      <MaterialReactTable table={table} />
+    </>
+  );
 };
 
 export default ComplaintsPage;
+
