33f7141a6d003eb0519cb1bfc69c13f39f8c3cdd
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-05-09 19:07:55 -0600

    Add HeroAutocompleteInput component for enhanced input selection

---
+++ src/components/inputs/HeroAutocompleteInput.tsx
@@ -0,0 +1,68 @@
+import { Autocomplete, AutocompleteItem } from '@heroui/react';
+import { Controller, FieldValues } from 'react-hook-form';
+import { CustomInputProps, SelectItem } from '@/types';
+
+interface SelectInputProps<T extends FieldValues> extends CustomInputProps<T> {
+  items: SelectItem[];
+  isLoading?: boolean;
+  isVirtualized?: boolean;
+}
+
+export const HeroAutocompleteInput = <T extends FieldValues>(
+  props: SelectInputProps<T>,
+) => {
+  const {
+    control,
+    name,
+    rules,
+    items,
+    placeholder,
+    label,
+    isDisabled,
+    isLoading,
+    isVirtualized,
+    variant,
+    size,
+  } = props;
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({
+        field: { onChange, value, ref },
+        fieldState: { invalid, error },
+      }) => (
+        <Autocomplete
+          ref={ref}
+          selectedKey={value}
+          defaultItems={items}
+          onSelectionChange={(key) => {
+            onChange(key);
+          }}
+          size={size}
+          isInvalid={invalid}
+          errorMessage={error?.message}
+          validationBehavior="aria"
+          label={label}
+          placeholder={placeholder}
+          isDisabled={isDisabled}
+          variant={variant || 'flat'}
+          isLoading={isLoading}
+          isVirtualized={isVirtualized}
+          scrollShadowProps={{
+            isEnabled: false,
+          }}
+        >
+          {(item) => (
+            <AutocompleteItem key={item.key} textValue={item.value}>
+              {item.value}
+            </AutocompleteItem>
+          )}
+        </Autocomplete>
+      )}
+    />
+  );
+};
+
