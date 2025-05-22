cda8036bc2e839387a0159eb670ae27d7ecd519e
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-05-22 13:52:04 -0600

    Refactor IndexHistorial component for improved readability and formatting

--- src/phicargo/disponiblidad/equipos/index.jsx
+++ src/phicargo/disponiblidad/equipos/index.jsx
@@ -1,35 +1,40 @@
 import React, { useState, useEffect, useMemo } from 'react';
-import { Tabs, Tab, Card, CardBody } from "@heroui/react";
+import { Tabs, Tab, Card, CardBody } from '@heroui/react';
 import HistorialManiobrasVehiculo from './historial_maniobras';
 import HistorialViajesVehiculo from './historial_viajes';
 import { ViajeProvider } from '@/phicargo/viajes/context/viajeContext';
 import { ManiobraProvider } from '@/phicargo/maniobras/context/viajeContext';
 
 const IndexHistorial = ({ vehicle_id }) => {
-
-    return (<>
-        <Tabs aria-label="Options" color="primary">
-            <Tab key="viajes" title="Viajes" disabled={true}>
-                <Card>
-                    <CardBody>
-                        <ViajeProvider>
-                            <HistorialViajesVehiculo vehicle_id={vehicle_id}></HistorialViajesVehiculo>
-                        </ViajeProvider>
-                    </CardBody>
-                </Card>
-            </Tab>
-            <Tab key="maniobras" title="Maniobras">
-                <Card>
-                    <CardBody>
-                        <ManiobraProvider>
-                            <HistorialManiobrasVehiculo vehicle_id={vehicle_id}></HistorialManiobrasVehiculo>
-                        </ManiobraProvider>
-                    </CardBody>
-                </Card>
-            </Tab>
-        </Tabs>
+  return (
+    <>
+      <Tabs aria-label="Options" color="primary">
+        <Tab key="viajes" title="Viajes" disabled={true}>
+          <Card>
+            <CardBody>
+              <ViajeProvider>
+                <HistorialViajesVehiculo
+                  vehicle_id={vehicle_id}
+                ></HistorialViajesVehiculo>
+              </ViajeProvider>
+            </CardBody>
+          </Card>
+        </Tab>
+        <Tab key="maniobras" title="Maniobras">
+          <Card>
+            <CardBody>
+              <ManiobraProvider>
+                <HistorialManiobrasVehiculo
+                  vehicle_id={vehicle_id}
+                ></HistorialManiobrasVehiculo>
+              </ManiobraProvider>
+            </CardBody>
+          </Card>
+        </Tab>
+      </Tabs>
     </>
-    );
+  );
 };
 
 export default IndexHistorial;
+
