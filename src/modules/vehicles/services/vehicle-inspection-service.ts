c9729276267cc80eb46705eecf1581aed534adf6
Guillermo Valverde <guillermovalverde42@gmail.com>
2025-06-19 13:49:04 -0600

    Refactor createVehicleInspection method to handle file uploads and improve error handling

--- src/modules/vehicles/services/vehicle-inspection-service.ts
+++ src/modules/vehicles/services/vehicle-inspection-service.ts
@@ -1,8 +1,9 @@
 import odooApi from '@/api/odoo-api';
 import type { VehicleInspection, VehicleInspectionCreate } from '../models';
-import type { VehicleInspectionApi } from '../models/api';
+import type { InspectionApi, VehicleInspectionApi } from '../models/api';
 import { VehicleInspectionAdapter } from '../adapters';
 import { AxiosError } from 'axios';
+import { uploadFiles } from '@/modules/core/services';
 
 export class VehicleInspectionService {
   static async getVehicleInspections(
@@ -29,17 +30,37 @@ export class VehicleInspectionService {
     }
   }
 
-  static async createVehicleInspection(
-    data: VehicleInspectionCreate,
-  ): Promise<void> {
+  static async createVehicleInspection({
+    files,
+    data,
+  }: {
+    data: VehicleInspectionCreate;
+    files?: File[];
+  }) {
     const body = VehicleInspectionAdapter.toVehicleInspectionApi(data);
     const url = `/vehicles/inspections/${data.vehicleId}`;
     try {
-      await odooApi.post<void>(url, body);
+      const response = await odooApi.post<InspectionApi>(url, body);
+
+      if (
+        response.status === 201 &&
+        files &&
+        files.length > 0 &&
+        response.data.incident_id
+      ) {
+        await uploadFiles(
+          files,
+          'incidents',
+          'x_driver_incidents',
+          response.data.incident_id,
+        );
+      }
     } catch (error) {
       console.error(error);
       if (error instanceof AxiosError) {
-        throw new Error(error.response?.data?.detail || 'Error al crear la inspección');
+        throw new Error(
+          error.response?.data?.detail || 'Error al crear la inspección',
+        );
       }
       throw new Error('Error al crear la inspección');
     }
