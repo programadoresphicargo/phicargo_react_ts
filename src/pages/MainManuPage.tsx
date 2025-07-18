6ab830b98a7ed4b9de9a496a3fb224fb046dfda7
programadoresphicargo <desarrollador@phi-cargo.com>
2025-07-18 15:55:35 -0600

    3:51 pm

--- src/pages/MainManuPage.tsx
+++ src/pages/MainManuPage.tsx
@@ -260,30 +260,30 @@ const MenuItem = ({ icon, label, link, }: MenuItemProps) => {
     <div
       onClick={handleClick}
       className="
-    flex
-    flex-col
-    items-center
-    justify-center
-    w-32
-    h-32
-    rounded-2xl
-    bg-white/10
-    shadow-lg
-    backdrop-blur-md
-    border
-    border-white/20
-    cursor-pointer
-    transition
-    duration-300
-    hover:bg-white/20
-  "
+      flex
+      flex-col
+      items-center
+      justify-center
+      w-32
+      h-32
+      rounded-2xl
+      bg-white/10
+      shadow-xl
+      backdrop-blur-lg
+      border
+      border-white/30
+      cursor-pointer
+      transition
+      duration-300
+      hover:bg-white/20
+      hover:shadow-2xl
+    "
     >
       <div className="mb-2.5">
         <img src={icon} alt={label} className="w-20 h-20 mb-1.5" />
       </div>
       <div className="text-xs font-bold text-white text-center">{label}</div>
     </div>
-
   );
 };
 
