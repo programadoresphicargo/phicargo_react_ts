8c42768b7ddc7ebfa6d01d6cdc725de3522aaee6
programadoresphicargo <desarrollador@phi-cargo.com>
2025-08-01 01:12:17 -0600

    1:12 AM

--- src/phicargo/reportes/sucursal_actual/sucursal.jsx
+++ src/phicargo/reportes/sucursal_actual/sucursal.jsx
@@ -1,173 +1,44 @@
 import { Button, Chip, DatePicker } from "@heroui/react";
-import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 import React, { useEffect, useState } from 'react';
-import { download, generateCsv, mkConfig } from 'export-to-csv';
-import { getLocalTimeZone, parseDate } from "@internationalized/date";
-
-import Badge from 'react-bootstrap/Badge';
-import { Box } from '@mui/material';
-import { Component } from "react";
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import ResponsiveAppBar from '@/phicargo/saldos_contabilidad/Navbar';
-import axios from 'axios';
-import odooApi from '@/api/odoo-api';
-import { toast } from "react-toastify";
-import { useDateFormatter } from "@react-aria/i18n";
-import { MRT_Localization_ES } from 'material-react-table/locales/es';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import SucursalActualUnidad from "./unidades";
+import SucursalActualOperador from "./operadores";
 
 const SucursalActual = () => {
 
-  const [isLoading, setisLoading] = useState('');
-  const [fechaInicio, setFechaInicio] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
-  const [fechaFin, setFechaFin] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
+  const [value, setValue] = React.useState('1');
 
-  const handleFechaInicioChange = (event) => {
-    setFechaInicio(event.target.value);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
-
-  const handleFechaFinChange = (event) => {
-    setFechaFin(event.target.value);
-  };
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    fetchData();
-  }, [fechaInicio, fechaFin]);
-
-  const fetchData = async () => {
-    try {
-      // Validar fechas
-      if (!fechaInicio || !fechaFin) {
-        throw new Error('Las fechas de inicio y fin son obligatorias.');
-      }
-
-      setisLoading(true);
-      const response = await odooApi.get(`/tms_travel/sucursal_actual/`);
-      setData(response.data);
-      console.log(response.data);
-    } catch (error) {
-      toast.error('Error al enviar los datos: ' + error);
-    } finally {
-      setisLoading(false);
-    }
-  };
-
-
-  const columns = [
-    { accessorKey: 'vehiculo', header: 'Vehiculo', },
-    {
-      accessorKey: 'sucursal', header: 'Sucursal', Cell: ({ cell }) => {
-        const valor = cell.getValue() || '';
-        var clase;
-
-        if (valor === 'Manzanillo (Sucursal)') {
-          clase = 'danger';
-        } else if (valor === 'Veracruz (Matriz)') {
-          clase = 'success';
-        } else {
-          clase = 'primary';
-        }
-
-        return (
-          <Chip color={clase} className="text-white">
-            {valor}
-          </Chip>
-        );
-      },
-    },
-    {
-      accessorKey: 'operador_asignado',
-      header: 'Operador asignado',
-      Cell: ({ cell }) => {
-        const valor = cell.getValue() || 'SIN OPERADOR ASIGNADO';
-
-        return (
-          <h1>
-            {valor}
-          </h1>
-        );
-      },
-    },
-    {
-      accessorKey: 'dias_en_sucursal', header: 'Días en sucursal'
-    },
-    { accessorKey: 'fecha_entrada_sucursal', header: 'Entrada a sucursal' },
-    { accessorKey: 'primer_viaje', header: 'Primer viaje' },
-    { accessorKey: 'fecha_ultimo_viaje', header: 'Fecha último viaje' },
-    { accessorKey: 'ultimo_viaje', header: 'Último viaje' },
-  ];
-
-  const table = useMaterialReactTable({
-    columns,
-    data,
-    state: { showProgressBars: isLoading },
-    enableGrouping: true,
-    enableGlobalFilter: true,
-    enableFilters: true,
-    localization: MRT_Localization_ES,
-    columnResizeMode: "onEnd",
-    initialState: {
-      density: 'compact',
-      expanded: false,
-      pagination: { pageSize: 80 },
-      showColumnFilters: true,
-    },
-    muiTablePaperProps: {
-      elevation: 0,
-      sx: {
-        borderRadius: '0',
-      },
-    },
-    muiTableHeadCellProps: {
-      sx: {
-        fontFamily: 'Inter',
-        fontWeight: 'Bold',
-        fontSize: '14px',
-      },
-    },
-    muiTableContainerProps: {
-      sx: {
-        maxHeight: 'calc(100vh - 185px)',
-      },
-    },
-    muiTableBodyCellProps: ({ row }) => ({
-      sx: {
-        backgroundColor: row.subRows?.length ? '#0456cf' : '#FFFFFF',
-        fontFamily: 'Inter',
-        fontWeight: 'normal',
-        fontSize: '14px',
-        color: row.subRows?.length ? '#FFFFFF' : '#000000',
-      },
-    }),
-    renderTopToolbarCustomActions: ({ table }) => (
-      <Box
-        sx={{
-          display: 'flex',
-          gap: '16px',
-          padding: '8px',
-          flexWrap: 'nowrap',
-          flexDirection: 'row',
-          alignItems: 'center',
-        }}
-      >
-
-        <h1
-          className="tracking-tight font-semibold lg:text-3xl bg-gradient-to-r from-[#0b2149] to-[#002887] text-transparent bg-clip-text"
-        >
-          Días en última sucursal por vehículo
-        </h1>
-
-      </Box >
-    ),
-  })
 
   return (
     <>
-      <MaterialReactTable
-        table={table}
-      />
+      <Box sx={{ width: '100%', typography: 'body1' }}>
+        <TabContext value={value}>
+          <Box sx={{ borderColor: 'divider', backgroundColor: '#002887', color: 'white' }}>
+            <TabList
+              onChange={handleChange}
+              aria-label="lab API tabs example"
+              textColor="inherit"
+              sx={{
+                '& .MuiTabs-indicator': {
+                  backgroundColor: 'white',
+                  height: '3px',
+                }
+              }}>
+              <Tab label="Operadores" value="1" sx={{ fontFamily: 'Inter' }} />
+              <Tab label="Unidades" value="2" sx={{ fontFamily: 'Inter' }} />
+            </TabList>
+          </Box>
+          <TabPanel value="1" sx={{ padding: 0, margin: 0 }}><SucursalActualOperador></SucursalActualOperador></TabPanel>
+          <TabPanel value="2" sx={{ padding: 0, margin: 0 }}><SucursalActualUnidad></SucursalActualUnidad></TabPanel>
+        </TabContext>
+      </Box>
     </>
   );
 };
---
+++ src/phicargo/reportes/sucursal_actual/unidades.jsx
@@ -0,0 +1,175 @@
+import { Button, Chip, DatePicker } from "@heroui/react";
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
+import React, { useEffect, useState } from 'react';
+import { download, generateCsv, mkConfig } from 'export-to-csv';
+import { getLocalTimeZone, parseDate } from "@internationalized/date";
+
+import Badge from 'react-bootstrap/Badge';
+import { Box } from '@mui/material';
+import { Component } from "react";
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import ResponsiveAppBar from '@/phicargo/saldos_contabilidad/Navbar';
+import axios from 'axios';
+import odooApi from '@/api/odoo-api';
+import { toast } from "react-toastify";
+import { useDateFormatter } from "@react-aria/i18n";
+import { MRT_Localization_ES } from 'material-react-table/locales/es';
+
+const SucursalActualUnidad = () => {
+
+  const [isLoading, setisLoading] = useState('');
+  const [fechaInicio, setFechaInicio] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
+  const [fechaFin, setFechaFin] = React.useState(parseDate(new Date().toISOString().split('T')[0]));
+
+  const handleFechaInicioChange = (event) => {
+    setFechaInicio(event.target.value);
+  };
+
+  const handleFechaFinChange = (event) => {
+    setFechaFin(event.target.value);
+  };
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchData();
+  }, [fechaInicio, fechaFin]);
+
+  const fetchData = async () => {
+    try {
+      // Validar fechas
+      if (!fechaInicio || !fechaFin) {
+        throw new Error('Las fechas de inicio y fin son obligatorias.');
+      }
+
+      setisLoading(true);
+      const response = await odooApi.get(`/tms_travel/sucursal_actual_unidades/`);
+      setData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      toast.error('Error al enviar los datos: ' + error);
+    } finally {
+      setisLoading(false);
+    }
+  };
+
+
+  const columns = [
+    { accessorKey: 'vehiculo', header: 'Vehiculo', },
+    {
+      accessorKey: 'sucursal', header: 'Sucursal', Cell: ({ cell }) => {
+        const valor = cell.getValue() || '';
+        var clase;
+
+        if (valor === 'Manzanillo (Sucursal)') {
+          clase = 'danger';
+        } else if (valor === 'Veracruz (Matriz)') {
+          clase = 'success';
+        } else {
+          clase = 'primary';
+        }
+
+        return (
+          <Chip color={clase} className="text-white">
+            {valor}
+          </Chip>
+        );
+      },
+    },
+    {
+      accessorKey: 'operador_asignado',
+      header: 'Operador asignado',
+      Cell: ({ cell }) => {
+        const valor = cell.getValue() || 'SIN OPERADOR ASIGNADO';
+
+        return (
+          <h1>
+            {valor}
+          </h1>
+        );
+      },
+    },
+    {
+      accessorKey: 'dias_en_sucursal', header: 'Días en sucursal'
+    },
+    { accessorKey: 'fecha_entrada_sucursal', header: 'Entrada a sucursal' },
+    { accessorKey: 'primer_viaje', header: 'Primer viaje' },
+    { accessorKey: 'fecha_ultimo_viaje', header: 'Fecha último viaje' },
+    { accessorKey: 'ultimo_viaje', header: 'Último viaje' },
+  ];
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    state: { showProgressBars: isLoading },
+    enableGrouping: true,
+    enableGlobalFilter: true,
+    enableFilters: true,
+    localization: MRT_Localization_ES,
+    columnResizeMode: "onEnd",
+    initialState: {
+      density: 'compact',
+      expanded: false,
+      pagination: { pageSize: 80 },
+      showColumnFilters: true,
+    },
+    muiTablePaperProps: {
+      elevation: 0,
+      sx: {
+        borderRadius: '0',
+      },
+    },
+    muiTableHeadCellProps: {
+      sx: {
+        fontFamily: 'Inter',
+        fontWeight: 'Bold',
+        fontSize: '14px',
+      },
+    },
+    muiTableContainerProps: {
+      sx: {
+        maxHeight: 'calc(100vh - 230px)',
+      },
+    },
+    muiTableBodyCellProps: ({ row }) => ({
+      sx: {
+        backgroundColor: row.subRows?.length ? '#0456cf' : '#FFFFFF',
+        fontFamily: 'Inter',
+        fontWeight: 'normal',
+        fontSize: '14px',
+        color: row.subRows?.length ? '#FFFFFF' : '#000000',
+      },
+    }),
+    renderTopToolbarCustomActions: ({ table }) => (
+      <Box
+        sx={{
+          display: 'flex',
+          gap: '16px',
+          padding: '8px',
+          flexWrap: 'nowrap',
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}
+      >
+
+        <h1
+          className="tracking-tight font-semibold lg:text-3xl bg-gradient-to-r from-[#0b2149] to-[#002887] text-transparent bg-clip-text"
+        >
+          Unidades
+        </h1>
+
+      </Box >
+    ),
+  })
+
+  return (
+    <>
+      <MaterialReactTable
+        table={table}
+      />
+    </>
+  );
+};
+
+export default SucursalActualUnidad;
