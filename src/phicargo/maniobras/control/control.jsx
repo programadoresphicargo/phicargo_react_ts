9bcc17b0e9d2cf16b099dc1669e734249795d646
programadoresphicargo <desarrollador@phi-cargo.com>
2025-11-03 09:34:46 -0600

    934am

--- src/phicargo/maniobras/control/control.jsx
+++ src/phicargo/maniobras/control/control.jsx
@@ -7,6 +7,8 @@ import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import { ManiobraProvider } from "../context/viajeContext";
+import CustomNavbar from "@/pages/CustomNavbar";
+import { pages } from '../pages';
 
 export default function control_maniobras() {
 
@@ -19,7 +21,7 @@ export default function control_maniobras() {
     return (
         <>
             <ManiobraProvider>
-                <ManiobrasNavBar />
+                <CustomNavbar pages={pages}></CustomNavbar>
                 <Box sx={{ width: '100%' }}>
                     <TabContext value={value}>
                         <Box
---
+++ src/phicargo/maniobras/pages.jsx
@@ -0,0 +1,20 @@
+export const pages = [
+ { name: 'CONTROL DE MANIOBRAS', path: '/control_maniobras' },
+ { name: 'CONTENEDORES', path: '/cartas-porte' },
+ {
+  name: 'NOMINAS',
+  subpages: [
+   { name: 'Nominas', path: '/nominas_viejas' },
+   { name: 'Nominas Nueva (No entrar)', path: '/nominas' },
+   { name: 'Precios', path: '/precios' },
+  ],
+ },
+ { name: 'Terminales', path: '/terminales' },
+ {
+  name: 'Reportes',
+  subpages: [
+   { name: 'CUMPLIMIENTO ESTATUS POR HORA', path: '/cumplimiento_estatus_ejecutivos_maniobras' },
+   { name: 'CUMPLIMIENTO ESTATUS POR ESTATUS', path: '/cumplimiento_estatus_maniobras' }
+  ],
+ },
+];
