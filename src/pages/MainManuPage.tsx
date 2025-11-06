17673a579c0a2031305a2e04b2acb04c203c4d9c
programadoresphicargo <desarrollador@phi-cargo.com>
2025-11-06 09:18:26 -0600

    918am

--- src/pages/MainManuPage.tsx
+++ src/pages/MainManuPage.tsx
@@ -50,7 +50,7 @@ const MainMenuPage = () => {
     <main
       style={{
         backgroundImage: `
-        linear-gradient(90deg, rgba(36, 36, 36, 0.95), rgba(40, 40, 40, 0.95)),
+        linear-gradient(90deg, rgba(11, 33, 73, 0.95), rgba(0, 40, 135, 0.95)),
         url(${fondo2})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
