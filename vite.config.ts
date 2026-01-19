aefa515144c8fcf0cae6b90b3260a56e02731d47
programadoresphicargo <desarrollador@phi-cargo.com>
2026-01-19 13:35:14 -0600

    1.35pm

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // o '0.0.0.0'
+    port: 5173,
+  },
+  
   plugins: [react()],
 
   resolve: {
