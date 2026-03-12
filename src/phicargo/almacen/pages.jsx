305b7365e93fdbafdd1ba42b731b9e6d7a51bdee
programadoresphicargo <desarrollador@phi-cargo.com>
2026-03-12 13:49:42 -0600

    1.49pm

--- src/phicargo/almacen/pages.jsx
+++ src/phicargo/almacen/pages.jsx
@@ -2,6 +2,6 @@ export const pages = [
  { name: 'Equipo de proteccion personal', path: '/solicitudes_epp', permiso: 515 },
  { name: 'Equipo de amarre', path: '/solicitudes_amarre', permiso: 516 },
  { name: 'Entrega de herramientas', path: '/solicitudes_herramientas' },
- { name: 'Llantas de refacción', path: '/solicitudes_llantas', permiso: 517 },
+ { name: 'Llantas de refacción', path: '/solicitudes_llantas' },
  { name: 'Inventario', path: '/inventario' },
 ];
