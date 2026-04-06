def0700d02b1aa42fbfd1c092d323c9c35e15c1f
programadoresphicargo <desarrollador@phi-cargo.com>
2026-04-06 13:49:41 -0600

    1.49pm

--- src/modules/maintenance/components/ConfirmDialog.tsx
+++ src/modules/maintenance/components/ConfirmDialog.tsx
@@ -6,26 +6,25 @@ import CheckIcon from '@mui/icons-material/Check';
 import { DatePickerElement } from 'react-hook-form-mui/date-pickers';
 import type { MaintenanceRecordStatus } from '../models';
 import { MuiSimpleModal } from '@/components';
-import { SelectElement } from 'react-hook-form-mui';
 import { useMaintenanceRecord } from '../hooks';
 
 interface RegisterDetailForm {
   status: MaintenanceRecordStatus;
-  checkOut: Dayjs;
+  checkIn: Dayjs;
 }
 
 const initialFormValues: RegisterDetailForm = {
-  status: 'completed',
-  checkOut: dayjs(),
+  status: 'pending',
+  checkIn: dayjs(),
 };
 
-interface CompleteDialogProps {
+interface ConfirmDialogProps {
   open: boolean;
   onClose: () => void;
   itemId: number;
 }
 
-const CompleteDialog = (props: CompleteDialogProps) => {
+const ConfirmDialog = (props: ConfirmDialogProps) => {
   const { open, onClose, itemId } = props;
 
   const {
@@ -40,7 +39,7 @@ const CompleteDialog = (props: CompleteDialogProps) => {
     editRegister(
       {
         id: itemId,
-        updatedItem: { status: data.status, checkOut: data.checkOut },
+        updatedItem: { status: data.status, checkIn: data.checkIn },
       },
       {
         onSuccess: () => {
@@ -56,7 +55,7 @@ const CompleteDialog = (props: CompleteDialogProps) => {
       open={open}
       onClose={onClose}
       disableEnforceFocus
-      header={<h2 className="text-center">Cambiar estado</h2>}
+      header={<h2 className="text-center">Confirmar reporte</h2>}
       customFooter={
         <>
           <Button
@@ -75,37 +74,16 @@ const CompleteDialog = (props: CompleteDialogProps) => {
             onClick={() => handleSubmit(onSubmit)()}
             loading={isPending}
           >
-            Actualizar
+            Confirmar
           </Button>
         </>
       }
     >
       <form className="flex flex-col gap-6 py-4 px-2">
-        <SelectElement
-          control={control}
-          name="status"
-          label="Estatus"
-          size="small"
-          slotProps={{
-            select: {
-              MenuProps: {
-                disablePortal: true,
-              }
-            }
-          }}
-          required
-          options={[
-            { label: 'Completado', id: 'completed' },
-            { label: 'Pendiente', id: 'pending' },
-            { label: 'Cancelado', id: 'cancelled' },
-          ]}
-          rules={{ required: 'Este campo es requerido' }}
-        />
-
         <DatePickerElement
           control={control}
-          name="checkOut"
-          label="Fecha de Salida"
+          name="checkIn"
+          label="Fecha de ingreso"
           inputProps={{ size: 'small' }}
           required
           rules={{ required: 'Este campo es requerido' }}
@@ -115,4 +93,4 @@ const CompleteDialog = (props: CompleteDialogProps) => {
   );
 };
 
-export default CompleteDialog;
+export default ConfirmDialog;
