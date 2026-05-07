9c22720e9eb846096231e1725404b186c9e4cf16
programadoresphicargo <desarrollador@phi-cargo.com>
2026-05-07 13:16:22 -0600

    1.16pm

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/inter';
 
 // import { getEnvVariables } from './phicargo/modules/cashflow-report/utils';
 // import { init as sentryInit } from '@sentry/react';
