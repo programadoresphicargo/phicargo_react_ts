22d169a5b3351f1984e76de6bc7e395fa9707e9d
programadoresphicargo <desarrollador@phi-cargo.com>
2026-05-13 13:19:01 -0600

    1.18pm

--- src/phicargo/viajes/cuenta_espejo/cuenta_espejo.tsx
+++ src/phicargo/viajes/cuenta_espejo/cuenta_espejo.tsx
@@ -14,19 +14,19 @@ export const copiarHTML = async (travel_id: number) => {
     console.log(data);
 
     const html = templateHTML
-      .replace('{{vehicle}}', data.vehicle.name)
-      .replace('{{marca}}', data.vehicle.marca)
-      .replace('{{modelo}}', data.vehicle.model)
-      .replace('{{employee}}', data.employee.name)
-      .replace('{{route}}', data.route.name)
-      .replace('{{modalidad}}', data.x_tipo_bel.toUpperCase())
-      .replace('{{contenedores}}', data.x_references)
-      .replace('{{cliente}}', data.partner.name)
-      .replace('{{origen}}', data.origen)
-      .replace('{{destino}}', data.destino)
-      .replace('{{client_order_ref}}', data.client_order_ref)
-      .replace('{{trailer1}}', data.trailer1.name)
-      .replace('{{trailer2}}', data.trailer2.name);
+    .replace('{{vehicle}}', data.vehicle?.name ?? '')
+    .replace('{{marca}}', data.vehicle?.marca ?? '')
+    .replace('{{modelo}}', data.vehicle?.model ?? '')
+    .replace('{{employee}}', data.employee?.name ?? '')
+    .replace('{{route}}', data.route?.name ?? '')
+    .replace('{{modalidad}}', data.x_tipo_bel?.toUpperCase() ?? '')
+    .replace('{{contenedores}}', data.x_references ?? '')
+    .replace('{{cliente}}', data.partner?.name ?? '')
+    .replace('{{origen}}', data.origen ?? '')
+    .replace('{{destino}}', data.destino ?? '')
+    .replace('{{client_order_ref}}', data.client_order_ref ?? '')
+    .replace('{{trailer1}}', data.trailer1?.name ?? '')
+    .replace('{{trailer2}}', data.trailer2?.name ?? '');
 
     await navigator.clipboard.write([
       new ClipboardItem({
