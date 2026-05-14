4c7f57883c9b08e3a64ea78f756583c7d927dd84
programadoresphicargo <desarrollador@phi-cargo.com>
2026-05-14 13:59:20 -0600

    1.59pm

--- src/phicargo/reportes/cumplimiento_general/maniobras/index_cumplimiento.tsx
+++ src/phicargo/reportes/cumplimiento_general/maniobras/index_cumplimiento.tsx
@@ -1,11 +1,3 @@
-import {
-    MaterialReactTable,
-    useMaterialReactTable,
-} from 'material-react-table';
-import React, { useEffect, useMemo, useState, useContext } from 'react';
-import Box from '@mui/material/Box';
-import odooApi from '@/api/odoo-api';
-import { ViajeProvider } from '@/phicargo/viajes/context/viajeContext';
 import { ManiobraProvider } from '@/phicargo/maniobras/context/viajeContext';
 import ReporteCumplimientoManiobra from './cumplimiento_maniobra';
 
