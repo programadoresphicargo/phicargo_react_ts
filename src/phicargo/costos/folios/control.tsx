0e90134294ae1e097e8b300c6c7a7b78b53217b3
programadoresphicargo <desarrollador@phi-cargo.com>
2026-05-20 09:38:51 -0600

    9.38am

--- src/phicargo/costos/folios/control.tsx
+++ src/phicargo/costos/folios/control.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import { Tabs, Tab, Card, CardBody } from "@heroui/react";
-import Maniobras from "./tabla";
 import { CostosExtrasProvider } from "../context/context";
 import FoliosCostosExtras from "./tabla";
 import CustomNavbar from "@/pages/CustomNavbar";
-import { pages } from '../folios/pages';
+import { pages } from './pages';
 
 export default function control_maniobras() {
 
     return (
-        <div>
+        <>
             <CostosExtrasProvider>
                 <CustomNavbar pages={pages}></CustomNavbar>
                 <FoliosCostosExtras></FoliosCostosExtras>
             </CostosExtrasProvider>
-        </div>
+        </>
     );
 }
