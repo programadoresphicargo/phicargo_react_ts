b3a88aa29ef60e366eb33a49bc7d844076cfd77d
programadoresphicargo <desarrollador@phi-cargo.com>
2026-06-03 13:43:22 -0600

    1.43pm

--- src/phicargo/inventarioti/celulares/asignacion/index.tsx
+++ src/phicargo/inventarioti/celulares/asignacion/index.tsx
@@ -1,14 +1,11 @@
 
-import { InventarioProvider } from '../../contexto/contexto';
 import Asignaciones from './asignaciones_celular';
 
 const AsignacionActivos = () => {
 
   return (
     <>
-      <InventarioProvider>
-        <Asignaciones></Asignaciones>
-      </InventarioProvider >
+      <Asignaciones></Asignaciones>
     </>
   );
 };
--- src/phicargo/inventarioti/celulares/asignacion/index_computo.tsx
+++ src/phicargo/inventarioti/celulares/asignacion/index_computo.tsx
@@ -1,13 +1,10 @@
-import { InventarioProvider } from '../../contexto/contexto';
 import AsignacionesEquipoComputo from './asignaciones_equipo_computo';
 
 const AsignacionActivos = () => {
 
   return (
     <>
-      <InventarioProvider>
-        <AsignacionesEquipoComputo></AsignacionesEquipoComputo>
-      </InventarioProvider >
+      <AsignacionesEquipoComputo></AsignacionesEquipoComputo>
     </>
   );
 };
