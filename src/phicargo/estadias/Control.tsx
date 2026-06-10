b17ac1424c456620488d504e23fdf5e409505e3c
programadoresphicargo <desarrollador@phi-cargo.com>
2026-06-10 13:11:17 -0600

    1.11pm

--- src/phicargo/estadias/Control.tsx
+++ src/phicargo/estadias/Control.tsx
@@ -2,13 +2,14 @@ import CustomNavbar from "@/pages/CustomNavbar";
 import { ViajeProvider } from "../viajes/context/viajeContext";
 import RegistrosEstadias from "./registros";
 import { CostosExtrasProvider } from "../costos/context/context";
+import { pages } from "../viajes/pages";
 
 export default function EstadiasIndex() {
     return (
         <>
             <ViajeProvider>
                 <CostosExtrasProvider>
-                    <CustomNavbar></CustomNavbar>
+                    <CustomNavbar pages={pages}></CustomNavbar>
                     <RegistrosEstadias></RegistrosEstadias>
                 </CostosExtrasProvider>
             </ViajeProvider>
