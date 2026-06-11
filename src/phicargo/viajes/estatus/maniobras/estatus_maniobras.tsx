5c505457e0302328d585b00e21a03b98ad8176d0
programadoresphicargo <desarrollador@phi-cargo.com>
2026-06-11 14:08:10 -0600

    2.08pm

--- src/phicargo/viajes/estatus/maniobras/estatus_maniobras.tsx
+++ src/phicargo/viajes/estatus/maniobras/estatus_maniobras.tsx
@@ -72,64 +72,62 @@ export default function SeguimientoSimpleManiobra({
     <EstatusHistorialAgrupado id_reporte={id_reporte} open={open} handleClose={handleClose}></EstatusHistorialAgrupado>
    )}
 
-   {data && (
-    <Accordion variant="splitted">
-     <AccordionItem
-      key="1"
-      aria-label="Accordion 1"
-      isDisabled={data?.estado_maniobra === "borrador" ? true : false}
-      title={
-       <div className="flex flex-col gap-1 items-start justify-center">
-        <strong>MANIOBRA DE {tipo_maniobra.toUpperCase()}</strong>
-        <h5 className="text-small tracking-tight text-default-400">
-         {data?.estado_maniobra === "borrador" ? "PROGRAMADA" : data?.estado_maniobra.toUpperCase()}
-        </h5>
-       </div>
-      }
-      startContent={
-       <Avatar
-        color={data?.estado_maniobra === "borrador" ? "default" : tipo_maniobra == "ingreso" ? "success" : "warning"}
-        isBordered
-        radius="full"
-        size="md"
-        src={"https://static.vecteezy.com/system/resources/previews/017/398/595/original/blue-cargo-container-png.png"}
-       />
-      }
-     >
+   <Accordion variant="splitted">
+    <AccordionItem
+     key="1"
+     aria-label="Accordion 1"
+     isDisabled={data?.estado_maniobra === "borrador" ? true : false}
+     title={
+      <div className="flex flex-col gap-1 items-start justify-center">
+       <strong>MANIOBRA DE {tipo_maniobra.toUpperCase()}</strong>
+       <h5 className="text-small tracking-tight text-default-400">
+        {data?.estado_maniobra === "borrador" ? "PROGRAMADA" : data?.estado_maniobra.toUpperCase()}
+       </h5>
+      </div>
+     }
+     startContent={
+      <Avatar
+       color={data?.estado_maniobra === "borrador" ? "default" : tipo_maniobra == "ingreso" ? "success" : "warning"}
+       isBordered
+       radius="full"
+       size="md"
+       src={"https://static.vecteezy.com/system/resources/previews/017/398/595/original/blue-cargo-container-png.png"}
+      />
+     }
+    >
 
-      {isLoading && (
-       <Progress isIndeterminate size="sm" color="primary"></Progress>
-      )}
+     {isLoading && (
+      <Progress isIndeterminate size="sm" color="primary"></Progress>
+     )}
 
-      {data?.estatus.map((step) => (
-       <div className="mt-3">
-        <Card isDisabled={step.fecha_hora ? false : true} isPressable fullWidth onPress={() => handleClickOpen(step.id_reporte)}>
-         <CardBody>
-          <div className="flex gap-5">
-           <Avatar
-            color={step.fecha_hora ? "success" : "default"}
-            isBordered
-            radius="full"
-            size="md"
-            src={VITE_ODOO_API_URL + `/assets/trafico/estatus_operativos/${step.imagen}`}
-           />
-           <div className="flex flex-col gap-1 items-start justify-center">
-            <h4 className="text-small font-semibold leading-none text-default-600">{step.nombre_estatus} {step.comentarios_estatus}</h4>
-            <h5 className="text-small tracking-tight text-default-400">
-             {step.fecha_hora && (
-              tiempoTranscurrido(step.fecha_hora)
-             )}
-            </h5>
-           </div>
+     {data?.estatus.map((step) => (
+      <div className="mt-3">
+       <Card isDisabled={step.fecha_hora ? false : true} isPressable fullWidth onPress={() => handleClickOpen(step.id_reporte)}>
+        <CardBody>
+         <div className="flex gap-5">
+          <Avatar
+           color={step.fecha_hora ? "success" : "default"}
+           isBordered
+           radius="full"
+           size="md"
+           src={VITE_ODOO_API_URL + `/assets/trafico/estatus_operativos/${step.imagen}`}
+          />
+          <div className="flex flex-col gap-1 items-start justify-center">
+           <h4 className="text-small font-semibold leading-none text-default-600">{step.nombre_estatus} {step.comentarios_estatus}</h4>
+           <h5 className="text-small tracking-tight text-default-400">
+            {step.fecha_hora && (
+             tiempoTranscurrido(step.fecha_hora)
+            )}
+           </h5>
           </div>
-         </CardBody>
-        </Card>
-       </div>
-      ))}
+         </div>
+        </CardBody>
+       </Card>
+      </div>
+     ))}
 
-     </AccordionItem>
-    </Accordion>
-   )}
+    </AccordionItem>
+   </Accordion>
 
   </>
  );
