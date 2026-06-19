b1ab14b7eae02851c488ea2f73b84ef781c6baa4
programadoresphicargo <desarrollador@phi-cargo.com>
2026-06-19 16:46:22 -0600

    4.46pm

--- src/phicargo/minutas/control_minutas.tsx
+++ src/phicargo/minutas/control_minutas.tsx
@@ -1,12 +1,11 @@
 import Minutas from "./minutas";
-import { MinutasProvider } from "./context";
 import CustomNavbar from "@/pages/CustomNavbar";
 
 export default function ControlMinutas() {
     return (
-        <MinutasProvider>
+        <>
             <CustomNavbar></CustomNavbar>
             <Minutas />
-        </MinutasProvider>
+        </>
     );
 }
