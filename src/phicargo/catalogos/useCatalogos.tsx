baf24d5b4f084aa4739f7066d0064829476a7ea5
programadoresphicargo <desarrollador@phi-cargo.com>
2026-06-23 11:00:46 -0600

    11.00am

--- src/phicargo/catalogos/useCatalogos.tsx
+++ src/phicargo/catalogos/useCatalogos.tsx
@@ -1,27 +1,42 @@
 import { useEffect, useState } from "react";
 import odooApi from "@/api/odoo-api";
+import { DriverBaseApi } from "@/modules/drivers/models/api";
+import { VehicleBaseApi } from "@/modules/vehicles/models/api";
+import { Modality } from "@/modules/drivers/models";
 
 export const useCatalogos = () => {
 
- const [drivers, setDrivers] = useState([]);
- const [tractores, setTractores] = useState([]);
- const [trailers, setTrailers] = useState([]);
- const [dollies, setDollies] = useState([]);
- const [motogeneradores, setMotogeneradores] = useState([]);
+ type FlotaOption = {
+  key: number,
+  label: string;
+  x_tipo_carga: string | null;
+  x_modalidad: Modality | null;
+ }
+
+ type DriverOption = {
+  key: number,
+  label: string;
+ }
+
+ const [drivers, setDrivers] = useState<DriverOption[]>([]);
+ const [tractores, setTractores] = useState<FlotaOption[]>([]);
+ const [trailers, setTrailers] = useState<FlotaOption[]>([]);
+ const [dollies, setDollies] = useState<FlotaOption[]>([]);
+ const [motogeneradores, setMotogeneradores] = useState<FlotaOption[]>([]);
  const [isLoading, setLoading] = useState(false);
 
- const getFlotaByTipo = async (tipo) => {
-  const response = await odooApi.get(`/vehicles/fleet_type/${tipo}`);
+ const getFlotaByTipo = async (tipo: string) => {
+  const response = await odooApi.get<VehicleBaseApi[]>(`/vehicles/fleet_type/${tipo}`);
   return response.data.map(item => ({
    key: item.id,
-   label: item.name,
+   label: item.name2,
    x_tipo_carga: item.x_tipo_carga,
    x_modalidad: item.x_modalidad
   }));
  };
 
  const getDrivers = async () => {
-  const response = await odooApi.get('/drivers/');
+  const response = await odooApi.get<DriverBaseApi[]>('/drivers/');
   return response.data.map(item => ({
    key: Number(item.id),
    label: item.name,
